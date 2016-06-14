77527500b2c0f5459525cea676522fcdc843eb07
Amandine PERRIN <amandine.perrin@pasteur.fr>
2016-06-14 16:22:20 +0200

    Put final log file and stdin/stdout of the last job to given result folder

--- scripts/post_pipeline.sh
+++ scripts/post_pipeline.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-if [ $# != 3 ]
+#$ -o $4/post-treatment.out
+#$ -e $4/post-treatment.err
+
+if [ $# != 4 ]
 then
-	echo "usage: $0 <dbpath> <lstfile> <date_init>"
+	echo "usage: $0 <dbpath> <lstfile> <date_init> <respath>"
 	exit
 fi
 
 dbpath=$1
 lstfile=$2
 dateinit=$3
+respath=$4
 
 date=`date +"%d-%m-%y.%H-%M"`
 
-cat $dbpath/log-*-$dateinit.out > log-$lstfile-$date.out
+cat $dbpath/log-*-$dateinit.out > $respath/log-$lstfile-$date.out
 rm prep-$lstfile*
 # rm $dbpath/prokka-gembase_$lstfile*
 rm $dbpath/log-*-$dateinit.out
