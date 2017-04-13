a435b47ebc7d27cb91b1238f597762417ed0cac0
Amandine PERRIN <amandine.perrin@pasteur.fr>
2017-04-13 10:16:15 +0200

    Check prokka results after run
    Prokka always returns a non-zero return code, and prints everything to stderr. So, we cannot check that it ran well with that.
    Instead, we check that the output files we need exist, and that the number of contigs, proteins, genes etc correspond between original file and result files

--- pythoncode/pipelinepackage/prokka_functions.py
+++ pythoncode/pipelinepackage/prokka_functions.py
@@ -81,6 +81,86 @@ def run_prokka(arguments):
                "--prefix {} {}").format(outdir, threads, name, gpath)
     logger.debug(cmd)
     try:
-        retcode = subprocess.call(shlex.split(cmd), stdout=FNULL, stderr=subprocess.STDOUT)
-    except:
-        print("error in prokka...")
+        retcode = subprocess.call(shlex.split(cmd), stdout=FNULL, stderr=prokf)
+        check_prokka(outdir, prok_logfile, name, gpath, nbcont)
+    except Exception as err:
+        logging.error("Error while trying to run prokka: {}".format(err))
+        prokf.close()
+        sys.exit(1)
+    prokf.close()
+
+
+def check_prokka(outdir, logf, name, gpath, nbcont):
+    """
+    Prokka writes everything to stderr, and always returns a non-zero return code. So, we
+    check if it ran well by checking the content of output directory.
+    """
+    if not os.path.isdir(outdir):
+        logger.error(("Prokka could not run properly. "
+                      "Look at {} for more information.").format(logf))
+    else:
+        oriname = os.path.basename(gpath)
+        tblfile = os.path.join(outdir, name + ".tbl")
+        faafile = os.path.join(outdir, name + ".faa")
+        ffnfile = os.path.join(outdir, name + ".ffn")
+        missing_file = False
+        if not os.path.isfile(tblfile):
+            logger.error(("{} {}: no .tbl file").format(name, oriname))
+            missing_file = True
+        if not os.path.isfile(faafile):
+            logger.error(("{} {}: no .faa file").format(name, oriname))
+            missing_file = True
+        if not os.path.isfile(ffnfile):
+            logger.error(("{} {}: no .ffn file").format(name, oriname))
+            missing_file = True
+        if not missing_file:
+            fnbcont, tnbCDS, tnbGene, tnbCRISPR = count_tbl(tblfile)
+            faaprot = count_headers(faafile)
+            ffngene = count_headers(ffnfile)
+            if nbcont != fnbcont:
+                logger.error(("{} {}: no matching number of contigs; "
+                              "nbcontig={}; in tbl ={}").format(name, oriname, nbcont, fnbcont))
+            if tnbCDS != faaprot:
+                logger.error(("{} {}: no matching number of proteins between tbl and faa; "
+                              "faa={}; in tbl ={}").format(name, oriname, faaprot, tnbCDS))
+            if tnbGene + tnbCRISPR != ffngene:
+                logger.error(("{} {}: no matching number of genes between tbl and ffn; "
+                        "ffn={}; in tbl ={}genes {}CRISPR").format(name, oriname,
+                                                                   ffngene, tnbCDS, tnbCRISPR))
+
+
+def count_tbl(tblfile):
+    """
+    Count the different features found in the tbl file:
+    - number of contigs
+    - number of proteins (CDS)
+    - number of genes (locus_tag)
+    - number of CRISPR arrays (repeat_region)
+    """
+    nbcont = 0
+    nbCDS = 0
+    nbGene = 0
+    nbCRISPR = 0
+    with open(tblfile, "r") as tblf:
+        for line in tblf:
+            if line.startswith(">"):
+                nbcont += 1
+            if "CDS" in line:
+                nbCDS += 1
+            if "locus_tag" in line:
+                nbGene += 1
+            if "repeat_region" in line:
+                nbCRISPR == 1
+    return nbcont, nbCDS, nbGene, nbCRISPR
+
+
+def count_headers(seqfile):
+    """
+    Count how many sequences there are in the given file
+    """
+    nbseq = 0
+    with open(seqfile, "r") as faaf:
+        for line in faaf:
+            if line.startswith(">"):
+                nbseq += 1
+    return nbseq
