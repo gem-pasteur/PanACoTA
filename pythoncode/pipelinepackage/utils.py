71d5a1234359a1a8132945d0e4742e58c70f732e
Amandine PERRIN <amandine.perrin@pasteur.fr>
2017-04-13 10:12:38 +0200

    Add util module, containing functions and classes used in main modules

---
+++ pythoncode/pipelinepackage/utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Util functions and classes.
+
+
+@author gem
+April 2017
+"""
+
+import logging
+
+
+class LessThanFilter(logging.Filter):
+    """
+    When using log, when a level is set to a handler, it is a minimum level. All
+    levels higher than it will be printed. If you want to print only until
+    a given level (no levels higher than the specified one), use this class like this:
+    handler.addFilter(LessThanFilter(level))
+    """
+    def __init__(self, level):
+        self._level = level
+        logging.Filter.__init__(self)
+
+    def filter(self, rec):
+        return rec.levelno < self._level
