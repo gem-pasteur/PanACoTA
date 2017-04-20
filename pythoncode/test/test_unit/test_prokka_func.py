2bcb7e1afc73ae0820f5437d9bacd0288dc556bc
Amandine PERRIN <amandine.perrin@pasteur.fr>
2017-04-20 16:52:35 +0200

    Add start of tests for check_prokka

--- pythoncode/test/test_unit/test_prokka_func.py
+++ pythoncode/test/test_unit/test_prokka_func.py
@@ -30,73 +30,73 @@ def test_count_headers():
     nb = pfunc.count_headers(seqfile)
     assert nb == 5
 
-# def test_check_prokka_no_outdir():
-#     """
+def test_check_prokka_no_outdir():
+    """
 
-#     """
-#     check log
+    """
+    check log
 
-# # def test_check_prokka_notbl():
-# #     """
-# #     Check that check_prokka returns false when a tbl file is missing
-# #     """
-# #     outdir = os.path.join("test", "data", "test_files")
-# #     name = "prokka_out_for_test-misstbl"
-# #     logf = "prokka.log"
-# #     gpath = "path/to/nogenome/original_name.fna"
-# #     nbcont = 7
-# #     assert not pfunc.check_prokka(outdir, logf, name, gpath, nbcont)
-
-
-# # def test_check_prokka_nofaa():
-# #     """
-# #     Check that check_prokka returns false when a tbl file is missing
-# #     """
-# #     outdir = os.path.join("test", "data", "test_files")
-# #     name = "prokka_out_for_test-missfaa"
-# #     logf = "prokka.log"
-# #     gpath = "path/to/nogenome/original_name.fna"
-# #     nbcont = 7
-# #     assert not pfunc.check_prokka(outdir, logf, name, gpath, nbcont)
-
-
-# def test_check_prokka_noffn():
+# def test_check_prokka_notbl():
 #     """
 #     Check that check_prokka returns false when a tbl file is missing
 #     """
 #     outdir = os.path.join("test", "data", "test_files")
-#     name = "prokka_out_for_test-missffn"
+#     name = "prokka_out_for_test-misstbl"
 #     logf = "prokka.log"
 #     gpath = "path/to/nogenome/original_name.fna"
 #     nbcont = 7
 #     assert not pfunc.check_prokka(outdir, logf, name, gpath, nbcont)
 
-# def test_check_prokka_wrong_cont():
-#     """
 
+# def test_check_prokka_nofaa():
+#     """
+#     Check that check_prokka returns false when a tbl file is missing
+#     """
+#     outdir = os.path.join("test", "data", "test_files")
+#     name = "prokka_out_for_test-missfaa"
+#     logf = "prokka.log"
+#     gpath = "path/to/nogenome/original_name.fna"
+#     nbcont = 7
+#     assert not pfunc.check_prokka(outdir, logf, name, gpath, nbcont)
+
+
+def test_check_prokka_noffn():
+    """
+    Check that check_prokka returns false when a tbl file is missing
+    """
+    outdir = os.path.join("test", "data", "test_files")
+    name = "prokka_out_for_test-missffn"
+    logf = "prokka.log"
+    gpath = "path/to/nogenome/original_name.fna"
+    nbcont = 7
+    assert not pfunc.check_prokka(outdir, logf, name, gpath, nbcont)
+
+def test_check_prokka_wrong_cont():
+    """
+
+    """
+    outdir = os.path.join("test", "data", "test_files")
+    name = "prokka_out_for_test"
+    logf = "prokka.log"
+    gpath = "path/to/nogenome/original_name.fna"
+    nbcont = 10
+    assert not pfunc.check_prokka(outdir, logf, name, gpath, nbcont)
+    + check log
+
+check wrong cds/prot
+check wrong gene
+
+
+# def test_check_prokka_ok():
+#     """
+#     Check that everything is ok with prokka results (tbl, faa and ffn files exist,
+#     and number of CDS, CRISPR and genes correspond between them)
 #     """
 #     outdir = os.path.join("test", "data", "test_files")
 #     name = "prokka_out_for_test"
 #     logf = "prokka.log"
 #     gpath = "path/to/nogenome/original_name.fna"
-#     nbcont = 10
-#     assert not pfunc.check_prokka(outdir, logf, name, gpath, nbcont)
-#     + check log
-
-# check wrong cds/prot
-# check wrong gene
-
-
-# # def test_check_prokka_ok():
-# #     """
-# #     Check that everything is ok with prokka results (tbl, faa and ffn files exist,
-# #     and number of CDS, CRISPR and genes correspond between them)
-# #     """
-# #     outdir = os.path.join("test", "data", "test_files")
-# #     name = "prokka_out_for_test"
-# #     logf = "prokka.log"
-# #     gpath = "path/to/nogenome/original_name.fna"
-# #     nbcont = 7
-# #     assert pfunc.check_prokka(outdir, logf, name, gpath, nbcont)
+#     nbcont = 7
+#     assert pfunc.check_prokka(outdir, logf, name, gpath, nbcont)
 
 
