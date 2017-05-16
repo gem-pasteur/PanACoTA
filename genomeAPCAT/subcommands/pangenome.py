6f96e6e43233fc53e3ba84b8d77b1c9f9dfa4e6d
Amandine PERRIN <amandine.perrin@pasteur.fr>
2017-05-16 15:59:28 +0200

    Add parser builder

--- genomeAPCAT/subcommands/pangenome.py
+++ genomeAPCAT/subcommands/pangenome.py
@@ -8,6 +8,9 @@ pan-genome is a subcommand of genomeAPCAT
 May 2017
 """
 
+import sys
+
+
 def main_from_parse(arguments):
     """
     Call main function from the arguments given by parser
@@ -29,16 +32,51 @@ def build_parser(parser):
     Method to create a parser for command-line options
     """
     import argparse
+    def perc_id():
+        try:
+            param = float(param)
+        except Exception:
+            msg = "argument -i percentage_id: invalid float value: {}".format(param)
+            raise argparse.ArgumentTypeError(msg)
+        if param < 0 or param > 1:
+            msg = ("The minimum %% of identity must be in [0, 1]. Invalid value: {}".format(param))
+            raise argparse.ArgumentTypeError(msg)
+        return param
     # Create command-line parser for all options and arguments to give
-    parser.add_argument(dest="list_file",
-                        help=("File containing the list of genome filenames to annotate (1 genome"
-                              " per line). Each genome is in multi-fasta format. You can "
-                              "specify the species name (4 characters) you want to give to each "
-                              "genome by adding it after the genome filename(s), separated "
-                              "by '::'. If not given, the species name will be the one given in "
-                              "'species' argument. You can also specify the date (4 digits) "
-                              "by adding '.' + your date choice after the genome "
-                              "filename(s), '::' and, if given, the species name."))
+    parser.add_argument("-l", dest="lstinfo_file", required=True,
+                        help=("File containing the list of all genomes to include in "
+                              "the pan-genome, 1 genome per line: it can be the "
+                              "LSTINFO-<list_file>.lst file of 'genomeAPCAT annotate' module."
+                              "Here, only the first column (genome name without extension) "
+                              "will be used. All proteins of all these genomes will be "
+                              "concatenated in a file called <dataset_name>.All.prt."))
+    parser.add_argument("-n", dest="dataset_name", required=True,
+                        help=("Name of the dataset which will be clustered (for exemple, "
+                              "SAEN1234 for 1234 Salmonella enterica genomes). This name will "
+                              "be used to name the protein databank, a well as the "
+                              "pangenome files."))
+    parser.add_argument("-d", dest="dbpath", required=True,
+                        help=("Path to the folder containing all protein files corresponding "
+                              "to the genomes of the dataset (output directory 'Proteins' "
+                              "of 'genomeAPCAT annotate' module)."))
+    parser.add_argument("-i", dest="min_id", required=True, type=perc_id,
+                        help=("Minimum sequence identity to be considered in the same "
+                              "cluster (number between 0 and 1)"))
+    parser.add_argument("-o", dest="outdir", required=True,
+                        help=("Output directory, where all results must be saved "
+                              "(including tmp folder)"))
+    parser.add_argument("-c", dest="clust_mode", choices=[0, 1, 2], default=1,
+                        help=("Choose the clustering mode: 0 for 'set cover', 1 for "
+                              "'single-linkage', 2 for 'CD-Hit'. Default "
+                              "is 'single-linkage' (1)"))
+    parser.add_argument("-s", dest="spedir",
+                        help=("use this option if you want to save the concatenated protein "
+                              "databank in another directory than the one containing all "
+                              "individual protein files ('Proteins' folder)."))
+    parser.add_argument("--threads", dest="threads",
+                        help=("add this option if you want to parallelize on several threads. "
+                              "Indicate on how many threads you want to parallelize. "
+                              "By default, it uses all threads in the computer."))
 
 
 def parse(parser, argu):
@@ -46,14 +84,7 @@ def parse(parser, argu):
     Parse arguments given to parser
     """
     args = parser.parse_args(argu)
-    return check_args(parser, args)
-
-
-def check_args(parser, args):
-  """
-  Check that arguments given to parser are as expected.
-  """
-  return args
+    return args
 
 
 if __name__ == '__main__':
