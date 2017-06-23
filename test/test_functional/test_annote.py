1dae084f432b4198fa495efceda44dc9a5df1a64
Amandine PERRIN <amandine.perrin@pasteur.fr>
2017-06-23 15:58:16 +0200

    update functionnal tests

--- test/test_functional/test_annote.py
+++ test/test_functional/test_annote.py
@@ -844,116 +844,134 @@ def test_main_onExistResDirForce(logger):
     shutil.rmtree(resdir, ignore_errors=True)
 
 
-# def test_annote_all():
-#     """
-#     Test that when we call the pipeline with all default parameters, all expected output files
-#     are created. Check the content of result files (LSTINFO, Genes, Proteins, Replicons),
-#     lstinfo, discarded and log files.
-#     Test that prokka files are in the right folder (result/tmp_files), and that tmp files
-#     are also in results/tmp_files (no tmp or prokka dir given, so default is used)
-#     """
-#     date = time.strftime("%m%y")
-#     fulldate = time.strftime("%Y-%m-%d")
-#     list_file = os.path.join("test", "data", "test_files", "list_genomes-func-test-default.txt")
-#     dbpath = os.path.join("test", "data", "genomes")
-#     respath = os.path.join("test", "data", "res_test_funcDefault")
-#     name = "GENO"
-#     # check that log files do not already exist. If they do, remove them
-#     log_files = [os.path.join(respath, "genomeAPCAT-annotate_list_genomes-func-test-default" + ext)
-#                  for ext in [".log", ".log.err", ".log.details"]]
-#     for lf in log_files:
-#         if os.path.isfile(lf):
-#             os.remove(lf)
-#     cmd = "genomeAPCAT annotate {} -d {} -r {} -n {}".format(list_file, dbpath, respath, name)
-#     ret = subprocess.call(cmd.split())
-#     assert ret == 0
-#     # Get output files
-#     lstfile = [os.path.join(respath, "LSTINFO-list_genomes-func-test-default.lst")]
-#     discfile = [os.path.join(respath, "discarded-list_genomes-func-test-default.lst")]
-#     QCfiles = [os.path.join(respath, qc + "list_genomes-func-test-default.png")
-#                for qc in ["QC_L90-", "QC_nb-contigs-"]]
-#     genomes = {"B2_A3_5.fasta-changeName.fna": "ESCO.1116.00002",
-#                "H299_H561.fasta-all.fna": "ESCO.1015.00001",
-#                "A_H738.fasta-all.fna": "GENO.1015.00001"}
-#     split5Nfiles = [os.path.join(respath, "tmp_files", g + "-split5N.fna") for g in genomes]
-#     proklogfiles = [os.path.join(respath, "tmp_files",
-#                                  g + "-split5N.fna-prokka.log")
-#                     for g in genomes]
-#     prokka_files = [os.path.join(respath, "tmp_files",
-#                                  g + "-split5N.fna-prokkaRes",
-#                                  genomes[g])
-#                     for g in genomes]
-#     lstinffiles = [os.path.join(respath, "LSTINFO", name + ".lst")
-#                    for name in list(genomes.values())]
-#     prtfiles = [os.path.join(respath, "Proteins", name + ".prt")
-#                 for name in list(genomes.values())]
-#     genfiles = [os.path.join(respath, "Genes", name + ".gen")
-#                 for name in list(genomes.values())]
-#     repfiles = [os.path.join(respath, "Replicons", name + ".fna")
-#                 for name in list(genomes.values())]
+def test_annote_all():
+    """
+    Test that when we call the pipeline with all default parameters, all expected output files
+    are created. Check the content of result files (LSTINFO, Genes, Proteins, Replicons),
+    lstinfo, discarded and log files.
+    Test that prokka files are in the right folder (result/tmp_files), and that tmp files
+    are also in results/tmp_files (no tmp or prokka dir given, so default is used)
+    """
+    date = time.strftime("%m%y")
+    fulldate = time.strftime("%Y-%m-%d")
+    list_file = os.path.join("test", "data", "test_files", "list_genomes-func-test-default.txt")
+    dbpath = os.path.join("test", "data", "genomes")
+    respath = os.path.join("test", "data", "res_test_funcDefault")
+    name = "GENO"
+    # check that log files do not already exist. If they do, remove them
+    log_files = [os.path.join(respath, "genomeAPCAT-annotate_list_genomes-func-test-default" + ext)
+                 for ext in [".log", ".log.err", ".log.details"]]
+    for lf in log_files:
+        if os.path.isfile(lf):
+            os.remove(lf)
+    cmd = "genomeAPCAT annotate {} -d {} -r {} -n {}".format(list_file, dbpath, respath, name)
+    ret = subprocess.call(cmd.split())
+    assert ret == 0
+    # Get output files
+    lstfile = [os.path.join(respath, "LSTINFO-list_genomes-func-test-default.lst")]
+    discfile = [os.path.join(respath, "discarded-list_genomes-func-test-default.lst")]
+    QCfiles = [os.path.join(respath, qc + "list_genomes-func-test-default.png")
+               for qc in ["QC_L90-", "QC_nb-contigs-"]]
+    genomes = {"B2_A3_5.fasta-changeName.fna": "ESCO.1116.00002",
+               "H299_H561.fasta-all.fna": "ESCO.1015.00001",
+               "A_H738.fasta-all.fna": "GENO.1015.00001"}
+    split5Nfiles = [os.path.join(respath, "tmp_files", g + "-split5N.fna") for g in genomes]
+    proklogfiles = [os.path.join(respath, "tmp_files",
+                                 g + "-split5N.fna-prokka.log")
+                    for g in genomes]
+    prokka_files = [os.path.join(respath, "tmp_files",
+                                 g + "-split5N.fna-prokkaRes",
+                                 genomes[g])
+                    for g in genomes]
+    lstinffiles = [os.path.join(respath, "LSTINFO", name + ".lst")
+                   for name in list(genomes.values())]
+    prtfiles = [os.path.join(respath, "Proteins", name + ".prt")
+                for name in list(genomes.values())]
+    genfiles = [os.path.join(respath, "Genes", name + ".gen")
+                for name in list(genomes.values())]
+    repfiles = [os.path.join(respath, "Replicons", name + ".fna")
+                for name in list(genomes.values())]
 
-#     # Check all output files exist
-#     for f in (log_files + lstfile + discfile + QCfiles + split5Nfiles +
-#               proklogfiles + lstinffiles + prtfiles + genfiles + repfiles):
-#         assert os.path.isfile(f)
-#     for f in prokka_files:
-#         assert os.path.isfile(f + ".tbl")
-#         assert os.path.isfile(f + ".faa")
-#         assert os.path.isfile(f + ".ffn")
+    # Check all output files exist
+    for f in (log_files + lstfile + discfile + QCfiles + split5Nfiles +
+              proklogfiles + lstinffiles + prtfiles + genfiles + repfiles):
+        assert os.path.isfile(f)
+    for f in prokka_files:
+        assert os.path.isfile(f + ".tbl")
+        assert os.path.isfile(f + ".faa")
+        assert os.path.isfile(f + ".ffn")
 
-#     # TODO: check next steps, update with changed lstfile.
-#     # Check content of result database files
-#     exp_dir = os.path.join("test", "data", "exp_files", "results_test_func-default")
-#     for f in (lstinffiles + prtfiles + genfiles + repfiles):
-#         exp_file = os.sep.join(f.split(os.sep)[-2:])
-#         exp_file_path = os.path.join(exp_dir, exp_file)
-#         with open(exp_file_path, "r") as expf, open(f, "r") as outf:
-#             for line_exp, line_out in zip(expf, outf):
-#                 assert line_exp == line_out
+    # Check content of result database files
+    exp_dir = os.path.join("test", "data", "exp_files", "results_test_func-default")
+    for f in (lstinffiles + prtfiles + genfiles + repfiles):
+        exp_file = os.sep.join(f.split(os.sep)[-2:])
+        exp_file_path = os.path.join(exp_dir, exp_file)
+        with open(exp_file_path, "r") as expf, open(f, "r") as outf:
+            for line_exp, line_out in zip(expf, outf):
+                assert line_exp == line_out
 
-#     # Check that log files (error, details and log) contain expected information
-#     with open(log_files[0], "r") as logf:
-#         infos = []
-#         for line in logf:
-#             assert line.startswith("[" + fulldate)
-#             assert line.count("::") == 2
-#             infos.append(line.strip().split("::")[-1].strip())
-#         assert "Reading genomes" in infos
-#         assert ("Cutting genomes at each stretch of at least 5 'N', and then, calculating "
-#                 "genome size, number of contigs and L90.") in infos
-#         assert ("Annotating all genomes with prokka") in infos
-#         assert ("Formatting all genomes") in infos
-#     with open(log_files[1], "r") as logf:
-#         infos = []
-#         for line in logf:
-#             assert line.startswith("[" + fulldate)
-#             assert line.count("::") == 2
-#             infos.append(line.strip().split("::")[-1].strip())
-#         assert ("toto.fst genome file does not exist. Its file will be ignored when "
-#                 "concatenating ['A_H738.fasta', 'toto.fst', 'genome1.fasta', "
-#                 "'genome.fst']") in infos
-#         assert ("genome.fst genome file does not exist. Its file will be ignored when "
-#                 "concatenating ['A_H738.fasta', 'toto.fst', 'genome1.fasta', "
-#                 "'genome.fst']") in infos
-#     # Check file content of genomes "-all" files
-#     # H299_H561.fasta and genome6.fasta
-#     exp_Hand6 = os.path.join(dbpath, "H299_H561-and-genome6.fna")
-#     Handg6 = os.path.join(dbpath, "H299_H561.fasta-all.fna")
-#     with open(exp_Hand6, "r") as expf, open(Handg6, "r") as outf:
-#         for line_exp, line_out in zip(expf, outf):
-#             assert line_exp == line_out
-#     os.remove(Handg6)
-#     # A_H738.fasta and genome1.fasta
-#     exp_Aand1 = os.path.join(dbpath, "A_H738-and-genome1.fna")
-#     Aand1 = os.path.join(dbpath, "A_H738.fasta-all.fna")
-#     with open(exp_Aand1, "r") as expf, open(Aand1, "r") as outf:
-#         for line_exp, line_out in zip(expf, outf):
-#             assert line_exp == line_out
-#     os.remove(Aand1)
-#     # Check lstinfo file content
-#     explst = os.path.join(exp_dir, "LSTINFO-list_genomes-func-test-default.lst")
-#     with open(lstfile[0], "r") as expf, open(explst, "r") as outf:
-#         for line_exp, line_out in zip(expf, outf):
-#             assert line_exp == line_out
-#     shutil.rmtree(respath)
+    # Check that log files (error, details and log) contain expected information
+    with open(log_files[0], "r") as logf:
+        infos = []
+        for line in logf:
+            assert line.startswith("[" + fulldate)
+            assert line.count("::") == 2
+            infos.append(line.split("::")[-1].strip())
+        assert "Reading genomes" in infos
+        assert ("Cutting genomes at each stretch of at least 5 'N', and then, calculating "
+                "genome size, number of contigs and L90.") in infos
+        assert ("Annotating all genomes with prokka") in infos
+        assert ("Formatting all genomes") in infos
+    with open(log_files[1], "r") as logf:
+        infos = []
+        for line in logf:
+            assert line.startswith("[" + fulldate)
+            assert line.count("::") == 2
+            infos.append(line.split("::")[-1].strip())
+        assert ("toto.fst genome file does not exist. Its file will be ignored when "
+                "concatenating ['A_H738.fasta', 'toto.fst', 'genome1.fasta', "
+                "'genome.fst']") in infos
+        assert ("genome.fst genome file does not exist. Its file will be ignored when "
+                "concatenating ['A_H738.fasta', 'toto.fst', 'genome1.fasta', "
+                "'genome.fst']") in infos
+    with open(log_files[2], "r") as logd:
+        infos = []
+        for line in logd:
+            assert line.startswith("[" + fulldate)
+            assert line.count("::") == 2
+            infos.append(line.split("::")[-1].strip())
+        print(infos)
+        assert ("Start annotating GENO.1015.00001 {}/A_H738.fasta-all.fna"
+                "-split5N.fna").format(os.path.join(respath, "tmp_files")) in infos
+        assert ("Start annotating ESCO.1015.00001 {}/H299_H561.fasta-all.fna"
+                "-split5N.fna").format(os.path.join(respath, "tmp_files")) in infos
+        assert ("Start annotating ESCO.1116.00002 {}/B2_A3_5.fasta-changeName.fna"
+                "-split5N.fna").format(os.path.join(respath, "tmp_files")) in infos
+        assert ("End annotating GENO.1015.00001 {}/A_H738.fasta-all.fna"
+                "-split5N.fna").format(os.path.join(respath, "tmp_files")) in infos
+        assert ("End annotating ESCO.1015.00001 {}/H299_H561.fasta-all.fna"
+                "-split5N.fna").format(os.path.join(respath, "tmp_files")) in infos
+        assert ("End annotating ESCO.1116.00002 {}/B2_A3_5.fasta-changeName.fna"
+                "-split5N.fna").format(os.path.join(respath, "tmp_files")) in infos
+    # Check file content of genomes "-all" files
+    # H299_H561.fasta and genome6.fasta
+    exp_Hand6 = os.path.join(dbpath, "H299_H561-and-genome6.fna")
+    Handg6 = os.path.join(dbpath, "H299_H561.fasta-all.fna")
+    with open(exp_Hand6, "r") as expf, open(Handg6, "r") as outf:
+        for line_exp, line_out in zip(expf, outf):
+            assert line_exp == line_out
+    os.remove(Handg6)
+    # A_H738.fasta and genome1.fasta
+    exp_Aand1 = os.path.join(dbpath, "A_H738-and-genome1.fna")
+    Aand1 = os.path.join(dbpath, "A_H738.fasta-all.fna")
+    with open(exp_Aand1, "r") as expf, open(Aand1, "r") as outf:
+        for line_exp, line_out in zip(expf, outf):
+            assert line_exp == line_out
+    os.remove(Aand1)
+    # Check lstinfo file content
+    explst = os.path.join(exp_dir, "LSTINFO-list_genomes-func-test-default.lst")
+    with open(lstfile[0], "r") as expf, open(explst, "r") as outf:
+        for line_exp, line_out in zip(expf, outf):
+            assert line_exp == line_out
+    shutil.rmtree(respath)
 
