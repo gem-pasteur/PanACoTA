1990f17844d430cb0d080203ab6094fc514605d9
Amandine PERRIN <amandine.perrin@pasteur.fr>
2017-06-28 16:59:45 +0200

    Move utility commands for pangenom in utils_pangenome.py

--- genomeAPCAT/pangenome_module/post_treatment.py
+++ genomeAPCAT/pangenome_module/post_treatment.py
@@ -11,8 +11,10 @@ April 2017
 import logging
 import os
 from genomeAPCAT import utils
+from genomeAPCAT import utils_pangenome as utilsp
 
 logger = logging.getLogger("pangenome.post-treat")
+
 
 def post_treat(families, pangenome):
     """
@@ -31,95 +33,9 @@ def post_treat(families, pangenome):
         - sum_0-mono-multi: should be equal to the total number of genomes in dataset
         - max_multi: maximum number of members from 1 genome
     """
-    fams_by_strain, families, all_strains = read_pangenome(pangenome, families)
+    fams_by_strain, families, all_strains = utilsp.read_pangenome(pangenome, families)
     res = open_outputs_to_write(fams_by_strain, families, all_strains, pangenome)
     # res = (qualis, quantis, summaries)
-
-
-def read_pangenome(pangenome, families=None):
-    """ Read pangenome information
-
-    Read pangenome according to what is available. First, check if python objects are available,
-    then if not, search for the binary file, and if not, read the text file.
-    pangenome: pangenome file
-    families: {num: [members]}
-    """
-    if families:
-        fams_by_strain, all_strains = get_fams_info(families)
-        if not os.path.isfile(pangenome + ".bin"):
-            logger.info("Saving all information to a binary file for later use")
-            utils.save_bin([fams_by_strain, families, all_strains], pangenome + ".bin")
-    elif os.path.isfile(pangenome + ".bin"):
-        logger.info("Retrieving info from binary file")
-        fams_by_strain, families, all_strains = utils.load_bin(pangenome + ".bin")
-    else:
-        fams_by_strain, families, all_strains = read_pan_file(pangenome)
-        logger.info("Saving all information to a binary file for later use")
-        utils.save_bin([fams_by_strain, families, all_strains], pangenome + ".bin")
-    return fams_by_strain, families, all_strains
-
-
-def get_fams_info(families):
-    """
-    input: families = {num: [members]}
-    output:
-    - fams_by_strain = {fam_num: {strain: [members], strain: [members]}}
-    - all_strains = list of all strains found in the pangenome
-    """
-    logger.info("Calculating pan_summary, pan_quali and pan_quanti")
-    fams_by_strain = {}
-    all_strains = []
-    for num, fam in families.items():
-        fams_by_strain[num] = {}
-        for gene in fam:
-            read_gene(gene, num, fams_by_strain, all_strains)
-    sort_all_strains = sorted(all_strains, key=lambda x: (x.split(".")[0], int(x.split(".")[-1])))
-    return fams_by_strain, sort_all_strains
-
-
-def read_pan_file(filein):
-    """ Read PanGenome file in 'filein', and put it into Python objects
-
-    Save all the python objects to a binary file, so that, if the script is ran several
-    times, there is no need to parse again all the file.
-
-    :param filein: name of the PanGenome file
-    :type filein: str
-    :returns: fams_by_strain = {fam_num: {strain: [members], strain: [members]}} and
-    families = {fam_num: [all members]}
-    all_strains = list of all strains found in the pangenome
-    :rtype: tuple(dict, dict, list)
-    """
-    logger.info("Calculating pan_summary, pan_quali and pan_quanti from pangenome file")
-    fams_by_strain = {}
-    families = {}
-    all_strains = []
-    nfam = 0
-    with open(filein, 'r') as coref:
-        for line in coref:
-            genes = line.strip().split()
-            fam_num = genes[0]
-            fams_by_strain[fam_num] = {}
-            genes_ok = genes[1:]
-            for gene in genes_ok:
-                read_gene(gene, fam_num, fams_by_strain, all_strains)
-            families[fam_num] = genes_ok
-            nfam += 1
-    sort_all_strains = sorted(all_strains, key=lambda x: int(x.split(".")[-1]))
-    return fams_by_strain, families, sort_all_strains
-
-
-def read_gene(gene, num, fams_by_strain, all_strains):
-    """
-    Read information from a given gene name, and save it to appropriate dicts
-    """
-    strain = ".".join(gene.split(".")[:3])
-    if strain in fams_by_strain[num]:
-        fams_by_strain[num][strain].append(gene)
-    else:
-        fams_by_strain[num][strain] = [gene]
-    if strain not in all_strains:
-        all_strains.append(strain)
 
 
 def open_outputs_to_write(fams_by_strain, families, all_strains, pangenome):
