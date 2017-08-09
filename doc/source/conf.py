65a92e2b0b67883d1074c4b8d143c1453a4632c3
Amandine PERRIN <amandine.perrin@pasteur.fr>
2017-08-09 17:25:54 +0200

    Add math feature to sphinx, to be able to use latex shortcuts for math operators

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,9 +31,9 @@ sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
               # 'sphinx.ext.doctest']
-              # 'sphinx.ext.mathjax',
+              # ,
               # 'sphinx.ext.ifconfig',
               # 'sphinx.ext.viewcode']
 
