3195fc22baac619b340f2035c01bc64f8059a0f3
aperrin <amandine.perrin@pasteur.fr>
2017-10-27 13:31:44 +0200

    Start of tests for utils_pangenome

--- test/test_unit/test_utils-pan.py
+++ test/test_unit/test_utils-pan.py
@@ -5,8 +5,6 @@
 Unit tests for the utils_pangenome submodule of genomeAPCAT
 """
 
-import pytest
-import os
 
 from genomeAPCAT import utils_pangenome as upan
 
@@ -15,3 +13,41 @@ def test_read_gene():
     """
     Check that when reading a given gene name, it extracts expected information
     """
+    gene = "ESCO.1016.00012.i012_00015"
+    num = "1"
+    fams_by_strain = {"1": {}}
+    all_strains = []
+    upan.read_gene(gene, num, fams_by_strain, all_strains)
+    assert all_strains == ["ESCO.1016.00012"]
+    assert fams_by_strain == {"1": {"ESCO.1016.00012": [gene]}}
+
+
+def test_read_gene_other_format():
+    """
+    Check that when reading a gene name which does not have the gembase
+    format, it still returns the right information
+    """
+    gene = "my_gene-name_other_0001"
+    num = "1"
+    fams_by_strain = {"1": {}}
+    all_strains = []
+    upan.read_gene(gene, num, fams_by_strain, all_strains)
+    assert all_strains == ["my_gene-name_other"]
+    assert fams_by_strain == {"1": {"my_gene-name_other": [gene]}}
+
+
+def test_read_gene_strain_known():
+    """
+    Check that when reading a gene name, and its corresponding
+    strain already exists in fams_by_strain and all_strains, it just adds
+    this new gene to fams_by_strain, and does nothing to all_strains
+    """
+    gene = "ESCO.1016.00012.i012_00015"
+    num = "1"
+    fams_by_strain = {"1": {"ESCO.1016.00012": ["ESCO.1016.00012.i001_01"]}}
+    all_strains = ["ESCO.1016.00012"]
+    upan.read_gene(gene, num, fams_by_strain, all_strains)
+    assert all_strains == ["ESCO.1016.00012"]
+    assert fams_by_strain == {"1": {"ESCO.1016.00012": ["ESCO.1016.00012.i001_01",
+                                                        gene]}}
+
