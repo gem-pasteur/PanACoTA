5e1b09cd265540fc2f4e4d39f55bf726c1914881
aperrin <amandine.perrin@pasteur.fr>
2017-11-03 16:03:16 +0100

    pylint, pep8 and docstrings for pangenome subcommand

--- genomeAPCAT/subcommands/pangenome.py
+++ genomeAPCAT/subcommands/pangenome.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 """
-pan-genome is a subcommand of genomeAPCAT
+pangenome is a subcommand of genomeAPCAT
 
 
 @author gem
@@ -16,6 +16,11 @@ import os
 def main_from_parse(args):
     """
     Call main function from the arguments given by parser
+
+    Parameters
+    ----------
+    args : argparse.Namespace
+        result of argparse parsing of all arguments in command line
     """
     main(args.lstinfo_file, args.dataset_name, args.dbpath, args.min_id, args.outdir,
          args.clust_mode, args.spedir, args.threads, args.outfile, args.verbose,
@@ -26,11 +31,44 @@ def main(lstinfo, name, dbpath, min_id, outdir, clust_mode, spe_dir, threads, ou
          verbose=0, quiet=False):
     """
     Main method, doing all steps:
+
     - concatenate all protein files
     - create database as ffindex
     - cluster all proteins
     - convert to pangenome file
     - creating summary and matrix of pangenome
+
+    Parameters
+    ----------
+    lstinfo : str
+        file with name of genomes to consider for pan in the first column, without extention.
+        Other columns are ignored
+    name : str
+        name given to the dataset. For example, ESCO44 for 44 *Escherichia coli* genomes.
+    dbpath : str
+        path to the folder containing all protein files (files called as the name of genome
+        given in lstinfo + ".prt"
+    min_id : float
+        Minimum percentage of identity between 2 proteins to put them in the same family
+    outdir : str
+        path to folder which will contain pangenome results and tmp files
+    clust_mode : [0, 1, 2]
+        0 for 'set cover', 1 for 'single-linkage', 2 for 'CD-Hit'
+    spe_dir : str or None
+        path to the folder where concatenated bank of proteins must be saved.
+        None to use the same folder as protein files
+    threads : int
+        Max number of threads to use
+    outfile : str or None
+        Name of the pangenome. None to use the default name
+    verbose : int
+        verbosity:
+
+        - defaut 0 : stdout contains DEBUG and INFO, stderr contains ERROR.
+        - 1: stdout contains (DEBUG) and INFO, stderr contains WARNING and ERROR
+        - 2: stdout contains (DEBUG), DETAIL and INFO, stderr contains WARNING and ERROR
+    quiet : bool
+        True if nothing must be sent to stdout/stderr, False otherwise
     """
     # import needed packages
     import logging
@@ -41,7 +79,7 @@ def main(lstinfo, name, dbpath, min_id, outdir, clust_mode, spe_dir, threads, ou
 
     # test if mmseqs is installed and in the path
     if not utils.check_installed("mmseqs"):  # pragma: no cover
-        logger.error("mmseqs is not installed. 'genomeAPCAT pangenome' cannot run.")
+        print("mmseqs is not installed. 'genomeAPCAT pangenome' cannot run.")
         sys.exit(1)
 
     os.makedirs(outdir, exist_ok=True)
@@ -49,7 +87,6 @@ def main(lstinfo, name, dbpath, min_id, outdir, clust_mode, spe_dir, threads, ou
     logfile_base = os.path.join(outdir, "genomeAPCAT-pangenome_" + name)
     level = logging.DEBUG
     utils.init_logger(logfile_base, level, '', verbose=verbose, quiet=quiet)
-    logger = logging.getLogger()
 
     # Build bank with all proteins to include in the pangenome
     prt_path = protf.build_prt_bank(lstinfo, dbpath, name, spe_dir, quiet)
@@ -63,9 +100,15 @@ def main(lstinfo, name, dbpath, min_id, outdir, clust_mode, spe_dir, threads, ou
 def build_parser(parser):
     """
     Method to create a parser for command-line options
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        parser to configure in order to extract command-line arguments
     """
     import argparse
     import multiprocessing
+
     def perc_id(param):
         try:
             param = float(param)
@@ -76,6 +119,7 @@ def build_parser(parser):
             msg = ("The minimum %% of identity must be in [0, 1]. Invalid value: {}".format(param))
             raise argparse.ArgumentTypeError(msg)
         return param
+
     def thread_num(param):
         try:
             param = int(param)
@@ -98,51 +142,51 @@ def build_parser(parser):
     # Create command-line parser for all options and arguments to give
     required = parser.add_argument_group('Required arguments')
     required.add_argument("-l", dest="lstinfo_file", required=True,
-                        help=("File containing the list of all genomes to include in "
-                              "the pan-genome, 1 genome per line: it can be the "
-                              "LSTINFO-<list_file>.lst file of 'genomeAPCAT annotate' module."
-                              "Here, only the first column (genome name without extension) "
-                              "will be used. All proteins of all these genomes will be "
-                              "concatenated in a file called <dataset_name>.All.prt."))
+                          help=("File containing the list of all genomes to include in "
+                                "the pan-genome, 1 genome per line: it can be the "
+                                "LSTINFO-<list_file>.lst file of 'genomeAPCAT annotate' module."
+                                "Here, only the first column (genome name without extension) "
+                                "will be used. All proteins of all these genomes will be "
+                                "concatenated in a file called <dataset_name>.All.prt."))
     required.add_argument("-n", dest="dataset_name", required=True,
-                        help=("Name of the dataset which will be clustered (for example, "
-                              "SAEN1234 for 1234 Salmonella enterica genomes). This name will "
-                              "be used to name the protein databank, a well as the "
-                              "pangenome files."))
+                          help=("Name of the dataset which will be clustered (for example, "
+                                "SAEN1234 for 1234 Salmonella enterica genomes). This name will "
+                                "be used to name the protein databank, a well as the "
+                                "pangenome files."))
     required.add_argument("-d", dest="dbpath", required=True,
-                        help=("Path to the folder containing all protein files corresponding "
-                              "to the genomes of the dataset (output directory 'Proteins' "
-                              "of 'genomeAPCAT annotate' module)."))
+                          help=("Path to the folder containing all protein files corresponding "
+                                "to the genomes of the dataset (output directory 'Proteins' "
+                                "of 'genomeAPCAT annotate' module)."))
     required.add_argument("-i", dest="min_id", required=True, type=perc_id,
-                        help=("Minimum sequence identity to be considered in the same "
-                              "cluster (number between 0 and 1)"))
+                          help=("Minimum sequence identity to be considered in the same "
+                                "cluster (number between 0 and 1)"))
     required.add_argument("-o", dest="outdir", required=True,
-                        help=("Output directory, where all results must be saved "
-                              "(including tmp folder)"))
+                          help=("Output directory, where all results must be saved "
+                                "(including tmp folder)"))
 
     optional = parser.add_argument_group('Optional arguments')
     optional.add_argument("-f", dest="outfile",
-                        help=("Use this option if you want to give the name of the pangenome "
-                              "output file (without path). Otherwise, by default, it is called "
-                              "PanGenome-mmseq_<given_dataset_name>.All.prt_<"
-                              "information_on_parameters>.lst"))
+                          help=("Use this option if you want to give the name of the pangenome "
+                                "output file (without path). Otherwise, by default, it is called "
+                                "PanGenome-mmseq_<given_dataset_name>.All.prt_<"
+                                "information_on_parameters>.lst"))
     optional.add_argument("-c", dest="clust_mode", type=int, choices=[0, 1, 2], default=1,
-                        help=("Choose the clustering mode: 0 for 'set cover', 1 for "
-                              "'single-linkage', 2 for 'CD-Hit'. Default "
-                              "is 'single-linkage' (1)"))
+                          help=("Choose the clustering mode: 0 for 'set cover', 1 for "
+                                "'single-linkage', 2 for 'CD-Hit'. Default "
+                                "is 'single-linkage' (1)"))
     optional.add_argument("-s", dest="spedir",
-                        help=("use this option if you want to save the concatenated protein "
-                              "databank in another directory than the one containing all "
-                              "individual protein files ('Proteins' folder)."))
+                          help=("use this option if you want to save the concatenated protein "
+                                "databank in another directory than the one containing all "
+                                "individual protein files ('Proteins' folder)."))
     optional.add_argument("--threads", dest="threads", default=1, type=thread_num,
-                        help=("add this option if you want to parallelize on several threads. "
-                              "Indicate on how many threads you want to parallelize. "
-                              "By default, it uses 1 thread. Put 0 if you want to use "
-                              "all threads of your computer."))
+                          help=("add this option if you want to parallelize on several threads. "
+                                "Indicate on how many threads you want to parallelize. "
+                                "By default, it uses 1 thread. Put 0 if you want to use "
+                                "all threads of your computer."))
 
     helper = parser.add_argument_group('Others')
     helper.add_argument("-v", "--verbose", dest="verbose", action="count", default=0,
-                        help=("Increase verbosity in stdout/stderr."))
+                        help="Increase verbosity in stdout/stderr.")
     helper.add_argument("-q", "--quiet", dest="quiet", action="store_true", default=False,
                         help=("Do not display anything to stdout/stderr. log files will "
                               "still be created."))
@@ -153,6 +197,19 @@ def build_parser(parser):
 def parse(parser, argu):
     """
     Parse arguments given to parser
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        Parser to use to parse command-line arguments
+    argu : str
+        command-line given
+
+    Returns
+    -------
+    argparse.Namespace or None
+        The arguments parsed, updated according to some rules. Exit program
+        with error message if error occurs with arguments given.
     """
     args = parser.parse_args(argu)
     return args
@@ -160,7 +217,8 @@ def parse(parser, argu):
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(description=("Compute pan-genome"), add_help=False)
-    build_parser(parser)
-    OPTIONS = parse(parser, sys.argv[1:])
+
+    my_parser = argparse.ArgumentParser(description="Compute pan-genome", add_help=False)
+    build_parser(my_parser)
+    OPTIONS = parse(my_parser, sys.argv[1:])
     main_from_parse(OPTIONS)
