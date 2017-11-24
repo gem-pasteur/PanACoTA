5d3bf681797126d404f7812f160f6bb4c675055a
aperrin <amandine.perrin@pasteur.fr>
2017-11-24 16:27:50 +0100

    All tests for corepers module

--- test/test_unit/test_persistent.py
+++ test/test_unit/test_persistent.py
@@ -4,8 +4,85 @@
 """
 Unit tests for the persistent_functions submodule in corepers module
 """
+import os
 
 import genomeAPCAT.corepers_module.persistent_functions as persf
+
+
+PERS_PATH = os.path.join("test", "data", "persgenome")
+EXP_PATH = os.path.join(PERS_PATH, "exp_files")
+FAMS_BY_STRAIN = \
+    {'1': {'GEN4.1111.00001': ['GEN4.1111.00001.b0001_00001'],
+           'GENO.0817.00001': ['GENO.0817.00001.b0001_00002'],
+           'GENO.1216.00002': ['GENO.1216.00002.b0001_00001', 'GENO.1216.00002.i0001_00002'],
+           'GENO.1216.00003': ['GENO.1216.00003.i0001_00003']
+           },
+     '10': {'GEN4.1111.00001': ['GEN4.1111.00001.i0001_00004'],
+            'GENO.0817.00001': ['GENO.0817.00001.i0002_00004'],
+            'GENO.1216.00002': ['GENO.1216.00002.i0001_00005']
+            },
+     '11': {'GEN4.1111.00001': ['GEN4.1111.00001.i0001_00005'],
+            'GENO.0817.00001': ['GENO.0817.00001.i0002_00005'],
+            'GENO.1216.00002': ['GENO.1216.00002.i0001_00006']
+            },
+     '12': {'GEN4.1111.00001': ['GEN4.1111.00001.i0001_00008'],
+            'GENO.0817.00001': ['GENO.0817.00001.i0002_00010'],
+            'GENO.1216.00002': ['GENO.1216.00002.b0002_00009'],
+            'GENO.1216.00003': ['GENO.1216.00003.i0001_00004', 'GENO.1216.00003.i0001_01000']
+            },
+     '13': {'GENO.1216.00002': ['GENO.1216.00002.b0003_00011'],
+            'GENO.0817.00001': ['GENO.0817.00001.i0002_00010']},
+     '14': {'GENO.1216.00002': ['GENO.1216.00002.b0003_00012']},
+     '2': {'GEN4.1111.00001': ['GEN4.1111.00001.i0001_00003']},
+     '3': {'GEN4.1111.00001': ['GEN4.1111.00001.b0001_00009'],
+           'GENO.0817.00001': ['GENO.0817.00001.b0002_00011'],
+           'GENO.1216.00002': ['GENO.1216.00002.b0002_00010'],
+           'GENO.1216.00003': ['GENO.1216.00003.i0001_01010']
+           },
+     '4': {'GENO.0817.00001': ['GENO.0817.00001.b0001_00001']},
+     '5': {'GEN4.1111.00001': ['GEN4.1111.00001.i0001_00002'],
+           'GENO.0817.00001': ['GENO.0817.00001.b0002_00003'],
+           'GENO.1216.00002': ['GENO.1216.00002.i0001_00003'],
+           'GENO.1216.00003': ['GENO.1216.00003.i0080_00010']
+           },
+     '6': {'GEN4.1111.00001': ['GEN4.1111.00001.i0001_00006'],
+           'GENO.0817.00001': ['GENO.0817.00001.i0002_00006', 'GENO.0817.00001.i0002_00007'],
+           'GENO.1216.00002': ['GENO.1216.00002.i0001_00007']
+           },
+     '7': {'GENO.0817.00001': ['GENO.0817.00001.i0002_00008']},
+     '8': {'GEN4.1111.00001': ['GEN4.1111.00001.i0001_00007'],
+           'GENO.0817.00001': ['GENO.0817.00001.i0002_00009'],
+           'GENO.1216.00002': ['GENO.1216.00002.b0001_00008']
+           },
+     '9': {'GENO.1216.00002': ['GENO.1216.00002.i0001_00004']}
+     }
+
+FAMILIES = {'1': ['GEN4.1111.00001.b0001_00001', 'GENO.0817.00001.b0001_00002',
+                  'GENO.1216.00002.b0001_00001', 'GENO.1216.00002.i0001_00002',
+                  'GENO.1216.00003.i0001_00003'],
+            '10': ['GEN4.1111.00001.i0001_00004', 'GENO.0817.00001.i0002_00004',
+                   'GENO.1216.00002.i0001_00005'],
+            '11': ['GEN4.1111.00001.i0001_00005', 'GENO.0817.00001.i0002_00005',
+                   'GENO.1216.00002.i0001_00006'],
+            '12': ['GEN4.1111.00001.i0001_00008', 'GENO.0817.00001.i0002_00010',
+                   'GENO.1216.00002.b0002_00009', 'GENO.1216.00003.i0001_00004',
+                   'GENO.1216.00003.i0001_01000'],
+            '13': ['GENO.1216.00002.b0003_00011', 'GENO.0817.00001.i0002_00010'],
+            '14': ['GENO.1216.00002.b0003_00012'],
+            '2': ['GEN4.1111.00001.i0001_00003'],
+            '3': ['GEN4.1111.00001.b0001_00009', 'GENO.0817.00001.b0002_00011',
+                  'GENO.1216.00002.b0002_00010', 'GENO.1216.00003.i0001_01010'],
+            '4': ['GENO.0817.00001.b0001_00001'],
+            '5': ['GEN4.1111.00001.i0001_00002', 'GENO.0817.00001.b0002_00003',
+                  'GENO.1216.00002.i0001_00003', 'GENO.1216.00003.i0080_00010'],
+            '6': ['GEN4.1111.00001.i0001_00006', 'GENO.0817.00001.i0002_00006',
+                  'GENO.0817.00001.i0002_00007', 'GENO.1216.00002.i0001_00007'],
+            '7': ['GENO.0817.00001.i0002_00008'],
+            '8': ['GEN4.1111.00001.i0001_00007', 'GENO.0817.00001.i0002_00009',
+                  'GENO.1216.00002.b0001_00008'],
+            '9': ['GENO.1216.00002.i0001_00004']
+            }
+
 
 def test_uniq_mems():
     """
@@ -63,6 +140,144 @@ def test_mixed_empty(caplog):
               "strain3b": [],
               "strain4": [],
               "strain3": ["member3"],
-              "strain4": []}
+              "strain5": []}
     assert persf.mixed_family(family, 3)
     assert "problem, no members for strain" in caplog.text
+
+
+def test_not_mixed():
+    """
+    Test that it returns false when there are less than the required
+    number of genomes with exactly 1 member
+    """
+    family = {"strain1": ["member"],
+              "strain2": ["member2"],
+              "strain3": ["member3"],
+              "strain4": ["member4", "member4bis"]}
+    assert not persf.mixed_family(family, 4)
+
+
+def test_mixed_more():
+    """
+    Test that it returns true when there are more than the required
+    number of genomes with exactly 1 member
+    """
+    family = {"strain1": ["member"],
+              "strain2": ["member2"],
+              "strain3": ["member3"],
+              "strain4": ["member4"],
+              "strain5": ["member5"]}
+    assert persf.mixed_family(family, 3)
+
+
+def test_write_pers():
+    """
+    Test that output file is written as expected
+    """
+    fams = {9: ["member_3", "member_12", "other_member_2"],
+            3: ["member_10", "member_100", "member_1"],
+            10: ["member_1", "member_2", "member_3"],
+            1: ["my_protein_3", "my_protein_12", "my_protein_2"],
+            5: ["ESCO.1216.00003.i001_01001", "SAEN.0215.00003.i009_00001",
+                "ESCO.1017.00003.b001_00001", "ESCO.0812.00002.i002_02000",
+                "ESCO.0812.00003.i002_02000"]}
+    outfile = "test-persistent_families.txt"
+    persf.write_persistent(fams, outfile)
+    expfile = os.path.join(EXP_PATH, "exp_persgenome1.txt")
+    with open(outfile, "r") as outf, open(expfile, "r") as expf:
+        for line_out, line_exp in zip(outf, expf):
+            assert line_out == line_exp
+    os.remove(outfile)
+
+
+def test_get_core_strict(caplog):
+    """
+    Getting a core genome (4 genomes, all having exactly 1 member)
+    """
+    fams = persf.get_pers(FAMS_BY_STRAIN, FAMILIES, 4)
+    exp_fams = {num: mems for num, mems in FAMILIES.items() if num in ['3', '5']}
+    assert exp_fams == fams
+    assert ("The persistent genome contains 2 families "
+            "having at least 4 genomes in each family.") in caplog.text
+
+
+def test_get_core_multi(caplog):
+    """
+    Getting a multi core genome (4 genomes, having at least 1 member)
+    """
+    fams = persf.get_pers(FAMS_BY_STRAIN, FAMILIES, 4, multi=True)
+    exp_fams = {num: mems for num, mems in FAMILIES.items() if num in ['3', '5', '1', '12']}
+    assert exp_fams == fams
+    assert ("The persistent genome contains 4 families "
+            "having at least 4 genomes in each family.") in caplog.text
+
+
+def test_get_99pers_floor_strict(caplog):
+    """
+    Getting a strict persistent at floor(99%) -> at least 3 genomes with 1 member, others
+    absent.
+    """
+    fams = persf.get_pers(FAMS_BY_STRAIN, FAMILIES, 4, tol=0.99, floor=True)
+    exp_fams = {num: mems for num, mems in FAMILIES.items() if num in ['3', '5', '8', '10', '11']}
+    assert exp_fams == fams
+    assert ("The persistent genome contains 5 families "
+            "having at least 3 genomes in each family.") in caplog.text
+
+
+def test_get_99pers_floor_mixed(caplog):
+    """
+    Getting a mixed persistent at floor(99%) -> at least 3 genomes with 1 member, others
+    anything
+    """
+    fams = persf.get_pers(FAMS_BY_STRAIN, FAMILIES, 4, tol=0.99, floor=True, mixed=True)
+    exp_fams = {num: mems for num, mems in FAMILIES.items() if num in ['1', '3', '5', '8', '10',
+                                                                       '11', '12']}
+    assert exp_fams == fams
+    assert ("The persistent genome contains 7 families "
+            "having at least 3 genomes in each family.") in caplog.text
+
+
+def test_get_99pers_floor_multi(caplog):
+    """
+    Getting a multi persistent genome at floor(99%) -> at least 3 genomes (any number of members)
+    """
+    fams = persf.get_pers(FAMS_BY_STRAIN, FAMILIES, 4, tol=0.99, floor=True, multi=True)
+    exp_fams = {num: mems for num, mems in FAMILIES.items() if num in ['1', '3', '5', '6', '8',
+                                                                       '10', '11', '12']}
+    assert exp_fams == fams
+    assert ("The persistent genome contains 8 families "
+            "having at least 3 genomes in each family.") in caplog.text
+
+
+def test_get_99pers_strict(caplog):
+    """
+    Getting a persistent genome at 99% (ceil) -> 4 genomes with exactly 1member
+    """
+    fams = persf.get_pers(FAMS_BY_STRAIN, FAMILIES, 4, tol=0.99)
+    exp_fams = {num: mems for num, mems in FAMILIES.items() if num in ['3', '5']}
+    assert exp_fams == fams
+    assert ("The persistent genome contains 2 families "
+            "having at least 4 genomes in each family.") in caplog.text
+
+
+def test_get_99pers_mixed(caplog):
+    """
+    Getting a mixed persistent genome at 99% (ceil) -> 4 genomes with exactly 1member
+    """
+    fams = persf.get_pers(FAMS_BY_STRAIN, FAMILIES, 4, tol=0.99, mixed=True)
+    exp_fams = {num: mems for num, mems in FAMILIES.items() if num in ['3', '5']}
+    assert exp_fams == fams
+    assert ("The persistent genome contains 2 families "
+            "having at least 4 genomes in each family.") in caplog.text
+
+
+def test_get_99pers_multi(caplog):
+    """
+    Getting a multi persistent genome at 99% (ceil) -> 3 genomes with exactly 1member,
+    other with anything
+    """
+    fams = persf.get_pers(FAMS_BY_STRAIN, FAMILIES, 4, tol=0.99, multi=True)
+    exp_fams = {num: mems for num, mems in FAMILIES.items() if num in ['1', '3', '5', '12']}
+    assert exp_fams == fams
+    assert ("The persistent genome contains 4 families "
+            "having at least 4 genomes in each family.") in caplog.text
