35252f19c8d21dc53c7d6a99566507c11cf513d1
aperrin <amandine.perrin@pasteur.fr>
2017-11-24 17:04:13 +0100

    Add sphinx docstrings for corepers subcommand

--- genomeAPCAT/subcommands/corepers.py
+++ genomeAPCAT/subcommands/corepers.py
@@ -4,6 +4,14 @@
 """
 corepers is a subcommand of genomeAPCAT
 
+Generate a core genome (families containing 1 member in all genomes of the dataset)
+or a persistent genome (families with a given % of genomes having exactly 1 member).
+You can also allow:
+
+- mixed families: exactly 1 member in the given percentage of genomes, but the other genomes
+  can contain 0 or several members
+- multi families: allow several members in any genome.
+
 
 @author gem
 June 2017
@@ -11,23 +19,48 @@ June 2017
 
 import os
 
+import sys
+
 
 def main_from_parse(args):
     """
     Call main function from the arguments given by parser
+
+    Parameters
+    ----------
+    args : argparse.Namespace
+        result of argparse parsing of all arguments in command line
     """
     main(args.pangenome, args.tol, args.multi, args.mixed, outputfile=args.outfile,
-         floor=args.floor)
+         floor=args.floor, verbose=args.verbose, quiet=args.quiet)
 
 
-def main(pangenome, tol, multi, mixed, outputfile=None, floor=False):
-    """ Read pangenome and deduce Persistent genome according to the user criteria
+def main(pangenome, tol, multi, mixed, outputfile=None, floor=False, verbose=0, quiet=False):
+    """
+    Read pangenome and deduce Persistent genome according to the user criteria
 
-    pangenome: file containing pangenome
-    tol: min number of genomes present in a family to consider it as persistent
-    multi: True if multigenic families are allowed, False otherwise
-    multi: True if mixed families are allowed, False otherwise
-    outputfile: name of persistent genome file
+    Parameters
+    ----------
+    pangenome : str
+        file containing pangenome
+    tol : float
+        min % of genomes present in a family to consider it as persistent (between 0 and 1)
+    multi : bool
+        True if multigenic families are allowed, False otherwise
+    mixed : bool
+        True if mixed families are allowed, False otherwise
+    outputfile : str or None
+        Specific name for the generated persistent genome. If not given, default name is used.
+    floor : bool
+        Require at least floor(nb_genomes*tol) genomes if True, ceil(nb_genomes*tol) if False
+    verbose : int
+        verbosity:
+
+        - defaut 0 : stdout contains DEBUG and INFO, stderr contains ERROR.
+        - 1: stdout contains (DEBUG) and INFO, stderr contains WARNING and ERROR
+        - 2: stdout contains (DEBUG), DETAIL and INFO, stderr contains WARNING and ERROR
+    quiet : bool
+        True if nothing must be sent to stdout/stderr, False otherwise
     """
     # import needed packages
     import logging
@@ -39,7 +72,7 @@ def main(pangenome, tol, multi, mixed, outputfile=None, floor=False):
     path_pan, base_pan = os.path.split(pangenome)
     logfile_base = os.path.join(path_pan, "genomeAPCAT-corepers")
     level = logging.DEBUG
-    utils.init_logger(logfile_base, level, '') #, verbose=verbose, quiet=quiet)
+    utils.init_logger(logfile_base, level, '', verbose=verbose, quiet=quiet)
     logger = logging.getLogger()
 
     # Define output filename
@@ -68,6 +101,22 @@ def main(pangenome, tol, multi, mixed, outputfile=None, floor=False):
 def get_info(tol, multi, mixed, floor):
     """
     Get a string corresponding to the information that will be given to logger.
+
+    Parameters
+    ----------
+    tol : float
+        min % of genomes present in a family to consider it as persistent (between 0 and 1)
+    multi : bool
+        True if multigenic families are allowed, False otherwise
+    mixed : bool
+        True if mixed families are allowed, False otherwise
+    floor : bool
+        Require at least floor(nb_genomes*tol) genomes if True, ceil(nb_genomes*tol) if False
+
+    Returns
+    -------
+    str
+        Information to give to logger
     """
     if tol == 1:
         return "Will generate a CoreGenome."
@@ -96,6 +145,11 @@ def get_info(tol, multi, mixed, floor):
 def build_parser(parser):
     """
     Method to create a parser for command-line options
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        parser to configure in order to extract command-line arguments
     """
     import argparse
 
@@ -114,7 +168,7 @@ def build_parser(parser):
     # Create command-line parser for all options and arguments to give
     required = parser.add_argument_group('Required arguments')
     required.add_argument("-p", dest="pangenome", required=True,
-                        help=("PanGenome file (1 line per family, first column is fam number)"))
+                          help="PanGenome file (1 line per family, first column is fam number)")
 
     optional = parser.add_argument_group('Optional arguments')
     optional.add_argument("-t", "--tol", dest="tol", default=1, type=percentage,
@@ -132,11 +186,11 @@ def build_parser(parser):
                                 "or several members in the '1-tol'%% left, use the option -X "
                                 "instead of this one: -M and -X options are not compatible."))
     optional.add_argument("-X", dest="mixed", action='store_true',
-                          help=("Add this option if you want to allow families having several "
-                                "members only in '1-tol'%% of the genomes. In the other genomes, "
-                                "only 1 member exactly is allowed. This option is not compatible "
-                                "with -M (which is allowing multigenic families: having several "
-                                "members in any number of genomes)."))
+                          help="Add this option if you want to allow families having several "
+                               "members only in '1-tol'%% of the genomes. In the other genomes, "
+                               "only 1 member exactly is allowed. This option is not compatible "
+                               "with -M (which is allowing multigenic families: having several "
+                               "members in any number of genomes).")
     optional.add_argument("-o", dest="outfile",
                           help=("You can specify an output filename (without path)"
                                 " for the Persistent genome "
@@ -150,7 +204,7 @@ def build_parser(parser):
 
     helper = parser.add_argument_group('Others')
     helper.add_argument("-v", "--verbose", dest="verbose", action="count", default=0,
-                        help=("Increase verbosity in stdout/stderr."))
+                        help="Increase verbosity in stdout/stderr.")
     helper.add_argument("-q", "--quiet", dest="quiet", action="store_true", default=False,
                         help=("Do not display anything to stdout/stderr. log files will "
                               "still be created."))
@@ -161,13 +215,26 @@ def build_parser(parser):
 def check_args(parser, args):
     """
     Check that arguments given to parser are as expected.
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        The parser used to parse command-line
+    args : argparse.Namespace
+        Parsed arguments
+
+    Returns
+    -------
+    argparse.Namespace or None
+        The arguments parsed, updated according to some rules. Exit program
+        with error message if error occurs with arguments given.
     """
     if args.multi and args.mixed:
         parser.error("-M and -X options cannot be activated together. Choose if you want to:\n"
                      "- allow several members in any number of genomes of a family (-M)\n"
                      "- allow several members in only '1-tol'% of the genomes of a family "
                      "(other 'tol'% genomes must have exactly 1 member) (-X)")
-    if args.mixed and args.tol==1:
+    if args.mixed and args.tol == 1:
         parser.error("You are asking for mixed families, while asking for 100% of the genomes of "
                      "a family to have exactly one member, which is not compatible. Do you want "
                      "to \n- lower the percentage of genomes required to have exactly "
@@ -183,6 +250,18 @@ def check_args(parser, args):
 def parse(parser, argu):
     """
     Parse arguments given to parser
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        the parser used
+    argu : [str]
+        command-line given by user, to parse using parser
+
+    Returns
+    -------
+    argparse.Namespace
+        Parsed arguments
     """
     args = parser.parse_args(argu)
     return check_args(parser, args)
@@ -190,8 +269,8 @@ def parse(parser, argu):
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(description=("Compute core or persistent genome"),
-                                     add_help=False)
-    build_parser(parser)
-    OPTIONS = parse(parser, sys.argv[1:])
+    myparser = argparse.ArgumentParser(description="Compute core or persistent genome",
+                                       add_help=False)
+    build_parser(myparser)
+    OPTIONS = parse(myparser, sys.argv[1:])
     main_from_parse(OPTIONS)
