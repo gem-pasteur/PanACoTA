f1751d7f4297b92bb03346f93c0ece8cad85a3e0
Amandine PERRIN <amandine.perrin@pasteur.fr>
2017-11-27 12:32:24 +0100

    Add test running corepers from command line

--- test/test_functional/test_corepers.py
+++ test/test_functional/test_corepers.py
@@ -5,6 +5,7 @@ Functional tests for genomeAPCAT corepers subcommand
 """
 import glob
 import os
+import subprocess
 
 import genomeAPCAT.subcommands.corepers as corepers
 
@@ -276,6 +277,38 @@ def test_main_from_parse(caplog):
     assert "Generating Persistent genome of a dataset containing 4 genomes" in caplog.text
     assert ("The persistent genome contains 2 families having at least "
             "4 genomes in each family.") in caplog.text
+
+
+def test_main_all():
+    """
+    Test when calling corepers from command line, it runs and gives expected output files
+    """
+    cmd = "genomeAPCAT corepers -p {}".format(PAN)
+    ret = subprocess.call(cmd.split())
+    assert ret == 0
+
+    # Check creation of binary file for pangenome, and remove it
+    assert os.path.isfile(PAN + ".bin")
+    os.remove(PAN + ".bin")
+
+    # Check presence of persistent genome, and its content, and remove it
+    out_pers = os.path.join(TEST_PATH, "PersGenome_test_pan-for-corepers.txt_1.lst")
+    exp_pers = os.path.join(EXP_PATH, "exp_coregenome.txt")
+    assert os.path.isfile(out_pers)
+    with open(out_pers, "r") as outf, open(exp_pers, "r") as expf:
+        for line_out, line_exp in zip(outf, expf):
+            assert line_out == line_exp
+    os.remove(out_pers)
+
+    # Check presence of log files and remove them
+    logfile = os.path.join(TEST_PATH, "genomeAPCAT-corepers.log")
+    assert os.path.isfile(logfile)
+    assert os.path.isfile(logfile + ".details")
+    assert os.path.isfile(logfile + ".err")
+    # check no error
+    with open(logfile + ".err", "r") as errf:
+        assert errf.readlines() == []
+    remove_all(logfile + "*")
 
 
 def remove_all(pattern):
