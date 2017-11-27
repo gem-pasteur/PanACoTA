0df536d1e8de41833038ef44d99ce149d9e6761a
aperrin <amandine.perrin@pasteur.fr>
2017-11-27 11:40:43 +0100

    Add start of tests for corepers parser

---
+++ test/test_functional/test_corepers-parser.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Functional tests for the parser of corepers.py
+"""
+
+import pytest
+import argparse
+
+from genomeAPCAT.subcommands import corepers
+
+
