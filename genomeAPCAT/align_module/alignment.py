db11d566774ad11fc30ff6d94009ad965c06276f
Amandine PERRIN <amandine.perrin@pasteur.fr>
2018-01-10 10:55:53 +0100

    add print to debug CI

--- genomeAPCAT/align_module/alignment.py
+++ genomeAPCAT/align_module/alignment.py
@@ -420,6 +420,8 @@ def mafft_align(num_fam, prt_file, mafft_file, nbfprt, logger):
     error = "Problem while trying to align fam {}".format(num_fam)
     stdout = open(mafft_file, "w")
     ret = utils.run_cmd(cmd, error, stdout=stdout, logger=logger)
+    print(mafft_file)
+    print(ret)
     stdout.close()
     if not isinstance(ret, int):
         ret = ret.returncode
