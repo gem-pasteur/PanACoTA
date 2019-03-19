6736f820664ff9d39f9ee4a15175858893c9434d
Amandine PERRIN <amandine.perrin@pasteur.fr>
2019-03-19 12:32:17 +0100

    Update tests for installation from computer with prokka and quicktree

--- test/test_install/test_make_prokka-quicktree.py
+++ test/test_install/test_make_prokka-quicktree.py
@@ -2,20 +2,27 @@
 # coding: utf-8
 
 """
-Tests for make script, installing genomeAPCAT according to already existing dependencies
+Tests for make script, installing PanACoTA according to already existing dependencies
+From a computer with ubuntu, prokka and quicktree installed
 """
 import os
+import pytest
+import glob
 
 from . import utilities as utils
 
-
-def teardown_module():
-    """
-    Uninstall genomeAPCAT and installed dependencies
-    """
-    cmd = "python3 make clean"
-    error = "Error clean"
+@pytest.fixture
+def install_panacota():
+    cmd = "python3 make"
+    error = "Error installing"
     utils.run_cmd(cmd, error)
+
+
+def teardown_function(function):
+    """
+    Uninstall genomeAPCAT and installed dependencies at the end of each test
+    """
+    print("TEARDOWN\n")
     cmd = "python3 make uninstall"
     error = "Error uninstall"
     utils.run_cmd(cmd, error)
@@ -23,16 +30,18 @@ def teardown_module():
     print("cleaning repo")
 
 
-def test_build_prokka_quicktree():
+def test_install_prokka_quicktree():
     """
-    Test that when installing from a computer containing only prokka, it installs
-    genomeAPCAT (no barrnap), and returns the list of missing dependencies
+    Test that when installing from a computer containing only prokka and quicktree, it installs
+    PanACoTA, and returns the list of missing dependencies
     """
+    assert not utils.check_installed("barrnap")
+    assert not utils.check_installed("prodigal")
+    assert utils.check_installed("prokka")
+    assert not utils.check_installed("genomeAPCAT")
+    assert utils.check_installed('quicktree')
     cmd = "python3 make"
     error = "Error trying to install genomeAPCAT from base"
-    assert not utils.check_installed("barrnap")
-    assert utils.check_installed("prokka")
-    assert utils.check_installed('quicktree')
     utils.run_cmd(cmd, error)
     assert not utils.check_installed("barrnap")
     assert utils.check_installed("prokka")
@@ -44,11 +53,13 @@ def test_build_prokka_quicktree():
     with open(stdout, "w") as stdof:
         utils.run_cmd(cmd, err, stdout=stdof, stderr=stdof)
     with open(stdout, "r") as stdof:
-        lines = stdof.readlines()
-        assert "/usr/local/lib" in lines[7]
+        for line in stdof:
+            if line.startswith("Location"):
+                loc = line.split()[-1]
+                assert glob.glob(os.path.join(loc, r'genomeAPCAT*dist-info'))
     os.remove(stdout)
     logfile = "install.log"
-    content = ["Installing genomeAPCAT...",
+    content = ["Installing genomeAPCAT...", "DONE",
                "Some dependencies needed for some subcommands of genomeAPCAT are "
                "not installed. Here is the list of missing dependencies, and for what they are "
                "used. If you plan to use the subcommands hereafter, first install required "
@@ -57,12 +68,13 @@ def test_build_prokka_quicktree():
                "- mafft (to align persistent genomes in order to infer a phylogenetic tree "
                "after)",
                "See more information on how to download/install those softwares in README or in "
-               "documentation."]
+               "documentation.",
+               "- prodigal : for annotate subcommand, you at least need prodigal (for syntaxic "
+               "annotation only). If you even need functional annotation, also install prokka"]
     with open(logfile, "r") as logf:
-        lines_f = logf.readlines()
-        assert len(lines_f) == len(content)
-        for linef, linee in zip(lines_f, content):
-            assert linee in linef
+        logf_content = "".join(logf.readlines())
+        for linec in content:
+            assert linec in logf_content
     # Check that needed packages are installed
     assert utils.is_package_installed("argparse")
     assert utils.is_package_installed("progressbar")
@@ -72,7 +84,7 @@ def test_build_prokka_quicktree():
     os.remove(logfile)
 
 
-def test_upgrade():
+def test_upgrade(install_panacota):
     """
     Test upgrading genomeAPCAT when dependencies are still installed
     """
@@ -90,12 +102,12 @@ def test_upgrade():
     logfile = "install.log"
     with open(logfile, "r") as logf:
         lines = logf.readlines()
-        assert len(lines) == 1
+        assert len(lines) == 2
         assert "Upgrading genomeAPCAT" in lines[0]
-    os.remove(logfile)
+        assert "DONE" in lines[1]
 
 
-def test_uninstall_withdep():
+def test_uninstall_withdep(install_panacota):
     """
     Test uninstalling genomeAPCAT when dependencies are still installed
     """
@@ -117,6 +129,7 @@ def test_develop():
     """
     assert not utils.check_installed("genomeAPCAT")
     assert not utils.check_installed("barrnap")
+    assert utils.check_installed('quicktree')
     assert utils.check_installed("prokka")
     cmd = "python3 make develop"
     error = "Error develop"
@@ -130,11 +143,12 @@ def test_develop():
     stdout = "stdout_pip3show.out"
     with open(stdout, "w") as stdof:
         utils.run_cmd(cmd, err, stdout=stdof, stderr=stdof)
-    # Check that it was not installed
+    # Check installation in develop mode
     with open(stdout, "r") as stdof:
-        lines = stdof.readlines()
-        for line in lines:
-            assert "/usr/local/lib" not in line
+        for line in stdof:
+            if line.startswith("Location"):
+                loc = line.split()[-1]
+                assert glob.glob(os.path.join(loc, r'genomeAPCAT*egg-info'))
     os.remove(stdout)
     logfile = "install.log"
     content = ["Installing genomeAPCAT...",
@@ -146,13 +160,17 @@ def test_develop():
                "- mmseqs (for pangenome subcommand)",
                "- mafft (to align persistent genomes in order to infer a phylogenetic tree "
                "after)",
+               "- barrnap. If you use Prokka for functional annotation, it will not predict RNA.",
+               "- prodigal : for annotate subcommand, you at least need prodigal (for syntaxic "
+               "annotation only). If you even need functional annotation, also install prokka",
                "See more information on how to download/install those softwares in README or in "
                "documentation."]
+
     with open(logfile, "r") as logf:
-        lines_f = logf.readlines()
-        assert len(lines_f) == len(content)
-        for linef, linee in zip(lines_f, content):
-            assert linee in linef
+        logf_content = "".join(logf.readlines())
+        for linec in content:
+            assert linec in logf_content
+
     # Check that needed packages are installed
     assert utils.is_package_installed("argparse")
     assert utils.is_package_installed("progressbar")
@@ -162,93 +180,21 @@ def test_develop():
     os.remove(logfile)
 
 
-def test_clean():
-    """
-    Test cleaning dependencies installed by make script
-    """
-    assert not utils.check_installed("barrnap")
-    assert utils.check_installed("prokka")
-    assert utils.check_installed("genomeAPCAT")
-    cmd = "python3 make clean"
-    error = "Error trying to clean dependencies"
-    utils.run_cmd(cmd, error)
-    assert not utils.check_installed("barrnap")
-    assert utils.check_installed("prokka")
-    assert utils.check_installed("genomeAPCAT")
-    assert utils.check_installed('quicktree')
-    assert not os.path.isdir("binaries")
-    assert not os.path.isdir("dependencies")
-    logfile = "install.log"
-    with open(logfile, "r") as logf:
-        lines = logf.readlines()
-        assert len(lines) == 1
-        assert "Cleaning dependencies..." in lines[0]
-    os.remove(logfile)
-
-
-def test_uninstall_prokkadep():
-    """
-    Test uninstalling genomeAPCAT when dependencies are already removed
-    """
-    assert not utils.check_installed("barrnap")
-    assert utils.check_installed("prokka")
-    cmd = "python3 make uninstall"
-    error = "Error uninstalling"
-    utils.run_cmd(cmd, error)
-    assert not utils.check_installed("genomeAPCAT")
-    assert not utils.check_installed("barrnap")
-    assert utils.check_installed("prokka")
-
-
-def test_upgrade_notinstalled_prokkaponly():
-    """
-    Test upgrading genomeAPCAT when dependencies are not installed (only barrnap),
-    and genomeAPCAT is not installed. It just installs genomeAPCAT, without prokka dep
-    """
-    assert not utils.check_installed("barrnap")
-    assert utils.check_installed("prokka")
-    assert not utils.check_installed("genomeAPCAT")
-    cmd = "python3 make upgrade"
-    error = "Error upgrade"
-    utils.run_cmd(cmd, error)
-    assert not utils.check_installed("barrnap")
-    assert utils.check_installed("prokka")
-    assert utils.check_installed("genomeAPCAT")
-    logfile = "install.log"
-    with open(logfile, "r") as logf:
-        lines = logf.readlines()
-        assert len(lines) == 1
-        assert "Upgrading genomeAPCAT" in lines[0]
-    os.remove(logfile)
-
-
 def test_install_user():
     """
     Test that when installing from a computer containing only prokka, in user mode, it installs
     genomeAPCAT in /Users and returns list of dependencies
     """
-    cmd = "python3 make uninstall"
-    error = "Error uninstalling"
-    utils.run_cmd(cmd, error)
-    cmd = "python3 make --user"
-    error = "Error trying to install genomeAPCAT from base"
     assert not utils.check_installed("barrnap")
     assert utils.check_installed("prokka")
     assert not utils.check_installed("genomeAPCAT")
+    cmd = "python3 make --user"
+    error = "Error trying to install genomeAPCAT from base"
     utils.run_cmd(cmd, error)
     assert not utils.check_installed("barrnap")
     assert utils.check_installed("prokka")
     assert utils.check_installed("genomeAPCAT")
-    cmd = "pip3 show genomeAPCAT"
-    err = "error pip3"
-    stdout = "stdout_pip3show.out"
-    with open(stdout, "w") as stdof:
-        utils.run_cmd(cmd, err, stdout=stdof, stderr=stdof)
-    with open(stdout, "r") as stdof:
-        lines = stdof.readlines()
-        for line in lines:
-            assert "/usr/local/lib" not in line
-    os.remove(stdout)
+    # Check logfile content
     logfile = "install.log"
     content = ["Installing genomeAPCAT in user mode...",
                "Some dependencies needed for some subcommands of genomeAPCAT are "
@@ -261,12 +207,12 @@ def test_install_user():
                "See more information on how to download/install those softwares in README or in "
                "documentation."]
     with open(logfile, "r") as logf:
-        for linef, linee in zip(logf, content):
-            assert linee in linef
+        logf_content = "".join(logf.readlines())
+        for linec in content:
+            assert linec in logf_content
     # Check that needed packages are installed
     assert utils.is_package_installed("argparse")
     assert utils.is_package_installed("progressbar")
     assert utils.is_package_installed("numpy")
     assert utils.is_package_installed("matplotlib")
     assert utils.is_package_installed("Bio")
-    os.remove(logfile)
