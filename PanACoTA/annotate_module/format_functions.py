70a86a19140ba1be4aa5f9d4a14ed68ecb01259a
Amandine PERRIN <amandine.perrin@pasteur.fr>
2019-05-14 10:54:39 +0200

    move contig locus: b001 -> 001b

--- PanACoTA/annotate_module/format_functions.py
+++ PanACoTA/annotate_module/format_functions.py
@@ -676,8 +676,7 @@ def write_gene(gtype, locus_num, gene_name, product, crispr_num, cont_loc,
         gene_name = "crispr"
         product = "crispr-array"
         crispr_num += 1
-    locus_name = "{}.{}{}_{}".format(genome, cont_loc,
-                                     str(cont_num).zfill(4),
+    locus_name = "{}.{}{}_{}".format(genome, str(cont_num).zfill(4), cont_loc,
                                      str(locus_num).zfill(5))
     # If '|' character found in those fields, replace by '_' to avoid problems while parsing
     more_info = "| {} | {} | {}".format(product.replace("|", "_"),
