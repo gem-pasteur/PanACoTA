28b786656f8dffc39f7795df59932b943b3b8f71
Amandine PERRIN <amandine.perrin@pasteur.fr>
2019-09-05 11:59:29 +0200

    Add structure for prepare_module tests

---
+++ test/test_unit/test_prepare/test_download.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Unit tests for the download_genomes_func submodule in prepare module
+"""
+import os
+import logging
+
+import PanACoTA.prepare_module.download_genomes_func as downg
---
+++ test/test_unit/test_prepare/test_filter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Unit tests for the download_genomes_func submodule in prepare module
+"""
+import os
+import logging
+
+import PanACoTA.prepare_module.filter_genomes as filterg
