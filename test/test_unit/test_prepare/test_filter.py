22762e0c20a173decebf8e2c42163c3ac06cdd0a
Amandine PERRIN <amandine.perrin@pasteur.fr>
2019-09-12 12:04:59 +0200

    Add tests for prepare:filter_genomes_write_outputfiles

--- test/test_unit/test_prepare/test_filter.py
+++ test/test_unit/test_prepare/test_filter.py
@@ -7,12 +7,13 @@ Unit tests for the download_genomes_func submodule in prepare module
 import os
 import logging
 import shutil
+import pytest
 
 import PanACoTA.prepare_module.filter_genomes as filterg
 
 DATA_TEST_DIR = os.path.join("test", "data", "prepare")
 
-def test_write_output(caplog):
+def test_write_output():
     """
     Check that the files with kept genomes and discarded genomes are created
     """
@@ -22,8 +23,10 @@ def test_write_output(caplog):
                       for gen, gz in corresp_genomes.items()]
     genomes = {genome_file:["gname", "ori_name", "path_annotate", 12, 10, 1]
                for genome_file in sorted_genomes}
-    genomes["toto"] = ['totoname', 'ori toto', 'path_toto', 13, 5, 6]
+    toto_path = os.path.join(DATA_TEST_DIR, "genomes", "refseq", "bacteria", "toto", "toto.fna.gz")
+    genomes[toto_path] = ['totoname', 'ori toto', 'path_toto', 13, 5, 6]
     genomes_removed = {"genome": ["ref", 10]}
+    sorted_genomes.append(toto_path)
 
     # Define output directory for generated files
     outdir = os.path.join(DATA_TEST_DIR, "test_filter_write_output")
@@ -42,23 +45,165 @@ def test_write_output(caplog):
     assert os.path.isfile(discard_file)
 
     # Check content of out files
+    # File containing genomes kept, that will be used for annotation
     with open(list_file) as lf:
         # Check header
         assert "to_annotate\tgsize\tnb_conts\tL90" in lf.readline()
         # Check there are 3 genomes, with expected information
-        assert "path_annotate 12\t10\t1" in lf.readline()
         assert "path_annotate\t12\t10\t1" in lf.readline()
         assert "path_annotate\t12\t10\t1" in lf.readline()
         assert "path_annotate\t12\t10\t1" in lf.readline()
-        assert lf.readline() == "\n"
+        assert "path_toto\t13\t5\t6" in lf.readline()
+        assert lf.readline() == ""
+        assert lf.readline() == ""
 
+    # File with discarded genomes, and why they are discarded
     with open(discard_file) as df:
         # Check header
         assert "genome_name\tproblem_compared_with\tdist" in df.readline()
         # Check genome line
-        assert "genome" in df.readline()
+        assert "genome\tref\t10" in df.readline()
         # Check no more genomes
-        assert df.readline() == '\n'
+        assert df.readline() == ''
+        assert df.readline() == ''
 
     # Remove test folder
-    # shutil.rmtree(outdir)
+    shutil.rmtree(outdir)
+
+
+
+def test_write_output_no_discard():
+    """
+    Check that the files with kept genomes and discarded genomes are created, but
+    file with discarded genomes is empty
+    """
+    corresp_genomes = {"ACOR001": "ACOR001.0519.fna.gz", "ACOR002": "ACOR002.0519.fna.gz",
+                       "ACOR003": "ACOR003.0519.fna.gz"}
+    sorted_genomes = [os.path.join(DATA_TEST_DIR, "genomes", "refseq", "bacteria", gen, gz)
+                      for gen, gz in corresp_genomes.items()]
+    genomes = {genome_file:["gname", "ori_name", "path_annotate", 12, 10, 1]
+               for genome_file in sorted_genomes}
+    toto_path = os.path.join(DATA_TEST_DIR, "genomes", "refseq", "bacteria", "toto", "toto.fna.gz")
+    genomes[toto_path] = ['totoname', 'ori toto', 'path_toto', 13, 5, 6]
+    genomes_removed = {}
+    sorted_genomes.append(toto_path)
+
+    # Define output directory for generated files
+    outdir = os.path.join(DATA_TEST_DIR, "test_filter_write_output_no_discard")
+    os.makedirs(outdir)
+    gspecies = "Acetobacter_fabarum"
+    min_dist = 0.06
+
+    # Check everything works without error
+    assert filterg.write_outputfiles(genomes, sorted_genomes,
+                                     genomes_removed, outdir, gspecies, min_dist) == 0
+
+    # Check outfiles exist
+    list_file = os.path.join(outdir, "LSTINFO-Acetobacter_fabarum-filtered-0.06.txt")
+    discard_file = os.path.join(outdir, "discarded-by-minhash-Acetobacter_fabarum-0.06.txt")
+    assert os.path.isfile(list_file)
+    assert os.path.isfile(discard_file)
+
+    # Check content of out files
+    # File containing genomes kept, that will be used for annotation
+    with open(list_file) as lf:
+        # Check header
+        assert "to_annotate\tgsize\tnb_conts\tL90" in lf.readline()
+        # Check there are 3 genomes, with expected information
+        assert "path_annotate\t12\t10\t1" in lf.readline()
+        assert "path_annotate\t12\t10\t1" in lf.readline()
+        assert "path_annotate\t12\t10\t1" in lf.readline()
+        assert "path_toto\t13\t5\t6" in lf.readline()
+        assert lf.readline() == ""
+        assert lf.readline() == ""
+
+    # File with discarded genomes, and why they are discarded
+    with open(discard_file) as df:
+        # Check header
+        assert "genome_name\tproblem_compared_with\tdist" in df.readline()
+        # Check no genome
+        assert df.readline() == ''
+        assert df.readline() == ''
+
+    # Remove test folder
+    shutil.rmtree(outdir)
+
+
+def test_write_output_no_genome():
+    """
+    Check that the files with kept genomes and discarded genomes are created, but
+    are empty because tehre is no genome
+    """
+    corresp_genomes = {"ACOR001": "ACOR001.0519.fna.gz", "ACOR002": "ACOR002.0519.fna.gz",
+                       "ACOR003": "ACOR003.0519.fna.gz"}
+    sorted_genomes = []
+    genomes = {}
+    genomes_removed = {}
+
+    # Define output directory for generated files
+    outdir = os.path.join(DATA_TEST_DIR, "test_filter_write_output_no_genome")
+    os.makedirs(outdir)
+    gspecies = "Acetobacter_fabarum"
+    min_dist = 0.06
+
+    # Check everything works without error
+    assert filterg.write_outputfiles(genomes, sorted_genomes,
+                                     genomes_removed, outdir, gspecies, min_dist) == 0
+
+    # Check outfiles exist
+    list_file = os.path.join(outdir, "LSTINFO-Acetobacter_fabarum-filtered-0.06.txt")
+    discard_file = os.path.join(outdir, "discarded-by-minhash-Acetobacter_fabarum-0.06.txt")
+    assert os.path.isfile(list_file)
+    assert os.path.isfile(discard_file)
+
+    # Check content of out files
+    # File containing genomes kept, that will be used for annotation
+    with open(list_file) as lf:
+        # Check header
+        assert "to_annotate\tgsize\tnb_conts\tL90" in lf.readline()
+        # Check there is not any genome
+        assert lf.readline() == ""
+        assert lf.readline() == ""
+
+    # File with discarded genomes, and why they are discarded
+    with open(discard_file) as df:
+        # Check header
+        assert "genome_name\tproblem_compared_with\tdist" in df.readline()
+        # Check no genome
+        assert df.readline() == ''
+        assert df.readline() == ''
+
+    # Remove test folder
+    shutil.rmtree(outdir)
+
+
+def test_write_output_no_outdir(caplog):
+    """
+    Check that when outdir does not exist, program ends with error message
+    """
+    corresp_genomes = {"ACOR001": "ACOR001.0519.fna.gz", "ACOR002": "ACOR002.0519.fna.gz",
+                       "ACOR003": "ACOR003.0519.fna.gz"}
+    sorted_genomes = []
+    genomes_removed = {}
+
+    # Define output directory for generated files
+    outdir = os.path.join(DATA_TEST_DIR, "test_filter_write_output_no_outdir")
+    gspecies = "Acetobacter_fabarum"
+
+    # Check everything works without error
+    with pytest.raises(SystemExit):
+        filterg.write_outputfiles({}, sorted_genomes, genomes_removed, outdir, gspecies, 0.06)
+
+    # Check outfiles do not exist
+    list_file = os.path.join(outdir, "LSTINFO-Acetobacter_fabarum-filtered-0.06.txt")
+    discard_file = os.path.join(outdir, "discarded-by-minhash-Acetobacter_fabarum-0.06.txt")
+    assert not os.path.isfile(list_file)
+    assert not os.path.isfile(discard_file)
+
+    # Check output
+    assert not os.path.isdir(outdir)
+
+    # Check logs
+    caplog.set_level(logging.DEBUG)
+    assert ("The given output directory (test/data/prepare/test_filter_write_output_no_outdir) "
+            "does not exist. We cannot create output files there") in caplog.text
