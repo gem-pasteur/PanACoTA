d81db3fa6d6051a814202ebdc02e4f56416db379
Amandine PERRIN <amandine.perrin@pasteur.fr>
2019-09-12 14:24:33 +0200

    Only tests for write_output and sort_genomes_minhash

--- test/test_unit/test_prepare/test_filter.py
+++ test/test_unit/test_prepare/test_filter.py
@@ -209,47 +209,129 @@ def test_write_output_no_outdir(caplog):
             "does not exist. We cannot create output files there") in caplog.text
 
 
-def test_read_matrix():
+def test_sort_genomes_minhash():
     """
-    Test that matrix from minhash is correctly read and converted to python matrix
+    Test that a given set of genomes with their proprieties is sorted as expected
+    (genomes with too bad quality removed, others sorted by l90 and nbcont)
     """
-    genomes = {"genome1": ["g1_name", "g1_ori", "path_genome1", 1500, 5, 2],
+    genomes = {"genome1": ["g1_name", "g1_ori", "path_genome1", 123567, 200, 101],
                "genome2": ["g2_name", "g2_ori", "path_genome2", 20000, 3, 1],
                "genome3": ["g3_name", "g3_ori", "path_genome3", 25003, 52, 50],
-               "genome4": ["g4_name", "g4_ori", "path_genome4", 22012, 20, 10]
+               "genome4": ["g4_name", "g4_ori", "path_genome4", 123456, 1023, 11],
+               "genome5": ["g5_name", "g5_ori", "path_genome5", 22012, 20, 10],
+               "genome6": ["g6_name", "g6_ori", "path_genome6", 1500, 5, 2]
               }
-    sorted_genomes = ["genome1", "genome2", "genome3", "genome4"]
-    matrix_file = os.path.join(DATA_TEST_DIR, "test_files", "minhash_output.txt")
-
-    mat_sp = filterg.read_matrix(genomes, sorted_genomes, matrix_file)
-
-    # Check content of created matrix
-    assert len(mat_sp) == 6
-    assert mat_sp[0,1] ==0.5
-    assert mat_sp[0,2] == 0.06
-    assert mat_sp[0,3] == 0.005
-    assert mat_sp[1,2] == 0.09
-    assert mat_sp[1,3] == 0.7
-    assert mat_sp[2,3] == 0.08
+    max_l90 = 100
+    max_cont = 1000
+    sorted_genomes = filterg.sort_genomes_minhash(genomes, max_l90, max_cont)
+    assert sorted_genomes == ["genome2", "genome6", "genome5", "genome3"]
 
 
-def test_read_matrix_no_genome():
+def test_sketch_all(caplog):
     """
-    Test that matrix from minhash is correctly read and converted to python matrix
+
     """
-    genomes = {}
-    sorted_genomes = []
-    matrix_file = os.path.join(DATA_TEST_DIR, "test_files", "minhash_output_empty.txt")
-    # Create empty matrix file
-    open(matrix_file, "w").close()
 
-    # Read matrix
-    mat_sp = filterg.read_matrix(genomes, sorted_genomes, matrix_file)
 
-    # Check content of created matrix
-    assert len(mat_sp) == 0
+# def test_read_matrix():
+#     """
+#     Test that matrix from minhash is correctly read and converted to python matrix
+#     """
+#     genomes = {"genome2": ["g2_name", "g2_ori", "path_genome2", 20000, 3, 1],
+#                "genome6": ["g6_name", "g6_ori", "path_genome6", 1500, 5, 2],
+#                "genome5": ["g5_name", "g5_ori", "path_genome5", 22012, 20, 10],
+#                "genome3": ["g3_name", "g3_ori", "path_genome3", 25003, 52, 50]
+#               }
+#     sorted_genomes = ["genome2", "genome6", "genome5", "genome3"]
+#     matrix_file = os.path.join(DATA_TEST_DIR, "test_files", "minhash_output.txt")
 
-    # Remove empty matrix file
-    os.remove(matrix_file)
+#     mat_sp = filterg.read_matrix(genomes, sorted_genomes, matrix_file)
+
+#     # Check content of created matrix
+#     assert len(mat_sp) == 6
+#     assert mat_sp[0,1] == 1e-6
+#     assert mat_sp[0,2] == 0.07
+#     assert mat_sp[0,3] == 1e-3
+#     assert mat_sp[1,2] == 0.08
+#     assert mat_sp[1,3] == 1e-4
+#     assert mat_sp[2,3] == 5e-2
+
+
+# def test_read_matrix_no_genome():
+#     """
+#     Test that matrix from minhash is correctly read and converted to python matrix
+#     """
+#     genomes = {}
+#     sorted_genomes = []
+#     matrix_file = os.path.join(DATA_TEST_DIR, "test_files", "minhash_output_empty.txt")
+#     # Create empty matrix file
+#     open(matrix_file, "w").close()
+
+#     # Read matrix
+#     mat_sp = filterg.read_matrix(genomes, sorted_genomes, matrix_file)
+
+#     # Check content of created matrix
+#     assert len(mat_sp) == 0
+
+#     # Remove empty matrix file
+#     os.remove(matrix_file)
+
+# def test_mash_step():
+#     """
+#     Prepare mash step: for a given genome, keep only genomes at a correct distance
+#     (between min_dist and max_dist). Add others to 'genomes_removed'
+#     """
+#     # CREATE MATRIX
+#     nbgen = 4
+#     mat_sp = dok_matrix((nbgen, nbgen), dtype=float)
+#     mat_sp[0,1] = 1e-6
+#     mat_sp[0,2] = 0.07
+#     mat_sp[0,3] = 1e-3
+#     mat_sp[1,2] = 0.08
+#     mat_sp[1,3] = 1e-4
+#     mat_sp[2,3] = 5e-2
+
+#     # PREPARE MASH STEP
+#     # List of genomes ordered by quality. genome1 is the best genome, genome2 the worst
+#     to_try = ["genome2", "genome4", "genome3", "genome1"]
+#     corresp = {"genome1":0, "genome3":1, "genome4":2, "genome2":3}
+#     genomes_removed = {}
+#     min_dist = 1e-4
+#     max_dit = 0.06
+
+#     # Check everything goes well
+#     assert filterg.mash_step(to_try, corresp, mat_sp, genomes_removed, min_dist, max_dit) == 0
+#     assert to_try == ["genome4"]
+#     assert genomes_removed == {"genome3": ["genome1", 0.07], "genome2": ["genome1", 1e-6]}
+
+
+
+# def test_mash_step_wrong_sp():
+#     """
+#     Prepare mash step: for a given genome, keep only genomes at a correct distance
+#     (between min_dist and max_dist). Add others to 'genomes_removed'
+#     """
+#     # CREATE MATRIX
+#     nbgen = 4
+#     mat_sp = dok_matrix((nbgen, nbgen), dtype=float)
+#     mat_sp[0,1] = 1e-6
+#     mat_sp[0,2] = 0.07
+#     mat_sp[0,3] = 1e-3
+#     mat_sp[1,2] = 0.08
+#     mat_sp[3,1] = 1e-4
+#     mat_sp[2,3] = 5e-2
+
+#     # PREPARE MASH STEP
+#     # List of genomes ordered by quality. genome1 is the best genome, genome2 the worst
+#     to_try = ["genome2", "genome4", "genome3", "genome1"]
+#     corresp = {"genome1":0, "genome3":1, "genome4":2, "genome2":3}
+#     genomes_removed = {}
+#     min_dist = 1e-4
+#     max_dit = 0.06
+
+#     # Check everything goes well
+#     assert filterg.mash_step(to_try, corresp, mat_sp, genomes_removed, min_dist, max_dit) == 0
+#     assert to_try == ["genome2"]
+#     assert genomes_removed == {"genome3": ["genome1", 0.07], "genome4": ["genome1", 1e-6]}
 
 
