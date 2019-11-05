252efaaf0479904170159e3ded3fc9b1b7fcb6a1
Amandine PERRIN <amandine.perrin@pasteur.fr>
2019-11-05 10:12:05 +0100

    Update docstring and logs

--- PanACoTA/pangenome_module/protein_seq_functions.py
+++ PanACoTA/pangenome_module/protein_seq_functions.py
@@ -21,15 +21,15 @@ def build_prt_bank(lstinfo, dbpath, name, spedir, quiet):
     Parameters
     ----------
     lstinfo : str
-        1 line per genome, only 1st column considered here, as the genome name\
+        1 line per genome, only 1st column considered here, as the genome name
         without extension
     dbpath : str
-        Proteins folder, containing all proteins for each genome. Each genome has\
+        Proteins folder, containing all proteins for each genome. Each genome has
         its own protein file, called `<genome_name>.prt`.
     name : str
         dataset name, used to name the output databank: <outdir>/<name>.All.prt
     spedir : str or None
-        By default, output file is saved in dbpath directory. If it must be saved somewhere\
+        By default, output file is saved in dbpath directory. If it must be saved somewhere
         else, it is specified here.
     quiet : bool
         True if nothing must be written in stdout/stderr, False otherwise
@@ -46,17 +46,17 @@ def build_prt_bank(lstinfo, dbpath, name, spedir, quiet):
         outdir = spedir
     outfile = os.path.join(outdir, name + ".All.prt")
     if os.path.isfile(outfile):
-        logger.warning(("Protein bank {} already exists. "
-                        "It will be used by mmseqs.").format(outfile))
+        logger.warning((f"Protein bank {outfile} already exists. "
+                        "It will be used by mmseqs."))
         return outfile
-    logger.info("Building bank with all proteins to {}".format(name + ".All.prt"))
+    logger.info("Building bank with all proteins to {}".format(outfile + ".All.prt"))
     genomes = []
     with open(lstinfo) as lstf:
         for line in lstf:
             # skip header
             if "_name" in line:
                 continue
-            genome = line.strip().split()[0]
+            genome = line.strip().split()[0].strip()
             genomes.append(genome)
     all_names = [os.path.join(dbpath, gen + ".prt") for gen in genomes]
     if quiet:
