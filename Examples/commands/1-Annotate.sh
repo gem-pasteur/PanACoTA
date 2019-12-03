d07468a281e9bd7ded6953b73b7ce1b4e1595de0
Amandine PERRIN <amandine.perrin@pasteur.fr>
2019-12-03 14:11:58 +0100

    Add example results for annotation with prodigal

--- Examples/commands/1-Annotate.sh
+++ Examples/commands/1-Annotate.sh
@@ -1,2 +1,5 @@
-PanACoTA annotate -d Examples/genomes -r Examples/1-res-Annotate Examples/input_files/list_genomes.lst -n EXAM
+# With prokka
+PanACoTA annotate -d Examples/genomes -r Examples/1-res-Annotate-prokka Examples/input_files/list_genomes.lst -n EXAM
 
+# With prodigal
+PanACoTA annotate -d Examples/genomes_init -l Examples/input_files/list_genomes.lst -n EXEM -r Examples/1-res-Annotate-prodigal --prodigal --small
