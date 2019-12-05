5e433500c94ab4b150f36279823578d4689323d8
Amandine PERRIN <amandine.perrin@pasteur.fr>
2019-12-05 10:05:23 +0100

    Update Example results for annotate step

--- Examples/commands/1-Annotate.sh
+++ Examples/commands/1-Annotate.sh
@@ -2,4 +2,4 @@
 PanACoTA annotate -d Examples/genomes -r Examples/1-res-Annotate-prokka Examples/input_files/list_genomes.lst -n EXAM
 
 # With prodigal
-PanACoTA annotate -d Examples/genomes_init -l Examples/input_files/list_genomes.lst -n EXEM -r Examples/1-res-Annotate-prodigal --prodigal --small
+PanACoTA annotate -d Examples/genomes_init -l Examples/input_files/list_genomes.lst -n EXAM -r Examples/1-res-Annotate-prodigal --prodigal --small
