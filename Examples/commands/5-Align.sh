a5402006028f88fd8541f1899fb7b95f13242336
asetGem <amandine.perrin@pasteur.fr>
2021-06-15 12:38:25 +0200

    change command example align step

--- Examples/commands/5-Align.sh
+++ Examples/commands/5-Align.sh
@@ -2,4 +2,4 @@
 PanACoTA align -c Examples/4-corepers/PersGenome_PanGenome-GENO3.All.prt-clust-0.8-mode1.lst-all_1.lst -l Examples/2-res-prokka/LSTINFO-list_genomes.lst -n GENO3_1 -d Examples/2-res-prokka -o Examples/5-align
 
 # only run align step of example
-PanACoTA align -c Examples/input_files/align-input/coregenome-example.lst -l Examples/input_files/pan-input/LSTINFO-list_genomes.lst -n GENO3_1 -d Examples/input_files/pan-input -o Examples/5-align
+PanACoTA align -c Examples/input_files/align-input/coregenome-example.lst -l Examples/input_files/pan-input/LSTINFO-list_genomes.lst -n GENO3_1 -d Examples/input_files/pan-input -o Examples/5-align-alone
